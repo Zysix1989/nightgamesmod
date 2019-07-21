110ac81f5033e19c2089502e883421d8d22bae95
inproto451 <in.proto451@gmail.com>
2019-07-21 01:20:39 -0400

    Don't reconstruct the panel each time
    Panels are generally meant to be reused.  This change maintains the
    panel layout from construction and let's the underlying values update.
    Stats are reconstructed every time because the number of those can
    change.

--- NightgamesMod/nightgames/gui/GUIPlayerAttributes.java
+++ NightgamesMod/nightgames/gui/GUIPlayerAttributes.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.io.IOException;
-import java.util.ArrayList;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,14 +25,55 @@ class GUIPlayerAttributes {
     private Player player;
     private JPanel targetPanel;
     private JPanel panel;
+    private JPanel statsPanel;
+    private JTextPane appearance;
     private JToggleButton button;
+    private Integer fontSize;
 
     GUIPlayerAttributes(Player player, JPanel targetPanel, GUI refreshTarget) {
         this.player = player;
         this.targetPanel = targetPanel;
 
+        // resolution resolver
+
+        Integer height = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.85);
+        Integer width = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.85);
+        fontSize = 5;
+
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, 1));
+        panel.setPreferredSize(new Dimension(width/8, targetPanel.getHeight()));
+        if (width < 720) {
+            panel.setMaximumSize(new Dimension(height, width / 6));
+        }
+        statsPanel = new JPanel(new GridLayout(0, 3));
+        panel.add(statsPanel);
+        statsPanel.setPreferredSize(new Dimension(width/8, 200));
+
+        JSeparator sep = new JSeparator();
+        sep.setMaximumSize(new Dimension(panel.getWidth(), 2));
+        panel.add(sep);
+        statsPanel.setBackground(GUIColors.bgLight);
+
+        appearance = new JTextPane();
+        DefaultCaret caret = (DefaultCaret) appearance.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.NEVER_UPDATE);
+        appearance.setBackground(GUIColors.bgLight);
+        appearance.setEditable(false);
+        appearance.setContentType("text/html");
+
+        JScrollPane appearanceScrollPane = new JScrollPane(appearance);
+        appearanceScrollPane.setBackground(GUIColors.bgLight);
+        appearanceScrollPane.setOpaque(false);
+        appearanceScrollPane.getViewport().setOpaque(false);
+        JPanel appearancePanel = new JPanel(new GridLayout());
+        appearancePanel.setMaximumSize(new Dimension(width/8, 2000));
+        appearancePanel.setPreferredSize(new Dimension(width/8, 2000));
+
+        appearancePanel.setBackground(GUIColors.bgLight);
+        panel.add(appearancePanel);
+        appearancePanel.add(appearanceScrollPane);
+        panel.setBackground(GUIColors.bgLight);
 
         button = new JToggleButton("Status");
         button.addActionListener(arg0 -> {
@@ -52,34 +92,9 @@ class GUIPlayerAttributes {
     }
 
     void displayStatus() {
-        // resolution resolver
 
-        Integer height = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.85);
-        Integer width = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.85);
-        Integer fontsize = 5;
-
-        panel.removeAll();
-        panel.repaint();
-        panel.setPreferredSize(new Dimension(width/8, targetPanel.getHeight()));
-
-
-        if (width < 720) {
-            panel.setMaximumSize(new Dimension(height, width / 6));
-            System.out.println("STATUS PANEL");
-        }
-        JPanel statsPanel = new JPanel(new GridLayout(0, 3));
-
-        panel.add(statsPanel);
-        //statsPanel.setPreferredSize(new Dimension(400, 200));
-        statsPanel.setPreferredSize(new Dimension(width/8, 200));
-
-        JSeparator sep = new JSeparator();
-        sep.setMaximumSize(new Dimension(panel.getWidth(), 2));
-        panel.add(sep);
-        int count = 0;
-        statsPanel.setBackground(GUIColors.bgLight);
-        int descFontSize = fontsize - 1;
-        ArrayList<JLabel> attlbls = new ArrayList<>();
+        statsPanel.removeAll();
+        int descFontSize = fontSize - 1;
         for (Attribute a : Attribute.values()) {
             int amt = player.get(a);
             int pure = player.getPure(a);
@@ -87,8 +102,7 @@ class GUIPlayerAttributes {
                 if (amt == pure) {
                     JLabel label = new JLabel(String.format("<html><font face='Georgia' size=%d>%s: %s</font></html>", descFontSize, a.name(), amt));
                     label.setForeground(GUIColors.textColorLight);
-                    attlbls.add(count, label);
-                    statsPanel.add(attlbls.get(count++));
+                    statsPanel.add(label);
                 } else {
                     String attrColor;
                     String bonusColor;
@@ -104,21 +118,13 @@ class GUIPlayerAttributes {
                         descFontSize, a.name(), attrColor, amt, statBonusFontSize, bonusColor, amt - pure);
                     JLabel label = new JLabel(labelString);
                     label.setForeground(GUIColors.textColorLight);
-                    attlbls.add(count, label);
-                    statsPanel.add(attlbls.get(count++));
+                    statsPanel.add(label);
                 }
             }
         }
 
-        // statusText - body, clothing and status description
-        JTextPane statusText = new JTextPane();
-        DefaultCaret caret = (DefaultCaret) statusText.getCaret();
-        caret.setUpdatePolicy(DefaultCaret.NEVER_UPDATE);
-        statusText.setBackground(GUIColors.bgLight);
-        statusText.setEditable(false);
-        statusText.setContentType("text/html");
-        HTMLDocument doc = (HTMLDocument) statusText.getDocument();
-        HTMLEditorKit editorKit = (HTMLEditorKit) statusText.getEditorKit();
+        HTMLDocument doc = (HTMLDocument) appearance.getDocument();
+        HTMLEditorKit editorKit = (HTMLEditorKit) appearance.getEditorKit();
         Global.freezeRNG();
         try {
             editorKit.insertHTML(doc, doc.getLength(),
@@ -133,29 +139,5 @@ class GUIPlayerAttributes {
             e.printStackTrace();
         }
         Global.unfreezeRNG();
-        JScrollPane scrollPane = new JScrollPane(statusText);
-        scrollPane.setBackground(GUIColors.bgLight);
-        scrollPane.setOpaque(false);
-        scrollPane.getViewport().setOpaque(false);
-        JPanel currentStatusPanel = new JPanel(new GridLayout());
-//        panel.setPreferredSize(new Dimension(400, height));
-//        currentStatusPanel.setMaximumSize(new Dimension(400, 2000));
-//        currentStatusPanel.setPreferredSize(new Dimension(400, 2000));
-        panel.setPreferredSize(new Dimension(width/8, height));
-        currentStatusPanel.setMaximumSize(new Dimension(width/8, 2000));
-        currentStatusPanel.setPreferredSize(new Dimension(width/8, 2000));
-
-        currentStatusPanel.setBackground(GUIColors.bgLight);
-        panel.add(currentStatusPanel);
-        currentStatusPanel.add(scrollPane);
-        panel.setBackground(GUIColors.bgLight);
-
-        if (width < 720) {
-            currentStatusPanel.setSize(new Dimension(height, width / 6));
-            System.out.println("Oh god so tiny");
-        }
-        panel.revalidate();
-        panel.revalidate();
-        panel.repaint();
     }
 }
