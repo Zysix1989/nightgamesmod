a9d7333b0fc081fb89e4bfa55f80e5d3308d6716
inproto451 <in.proto451@gmail.com>
2019-07-27 22:16:42 -0400

    FEATURE: Clicking the portrait toggles it to a text appearance.
    This is keeping with the UI design document and will hopefully free up
    some real estate on the story panel.

---
+++ NightgamesMod/nightgames/gui/GUIAppearanceDescriptionPanel.java
@@ -0,0 +1,88 @@
+package nightgames.gui;
+
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.text.DefaultCaret;
+import javax.swing.text.html.HTMLDocument;
+import nightgames.characters.NPC;
+import nightgames.global.Global;
+
+class GUIAppearanceDescriptionPanel extends JPanel {
+
+    private HTMLDocument document;
+    private JTextPane text;
+
+    // It's frustrating to have to write this boilerplate, but otherwise this component will steal
+    // MouseEvents and it's not supposed to be selectable text.
+    private class AppearanceDescriptionMouseListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+            for (MouseListener m : getParent().getMouseListeners()) {
+                m.mouseClicked(mouseEvent);
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+            for (MouseListener m : getParent().getMouseListeners()) {
+                m.mousePressed(mouseEvent);
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+            for (MouseListener m : getParent().getMouseListeners()) {
+                m.mouseReleased(mouseEvent);
+            }
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            for (MouseListener m : getParent().getMouseListeners()) {
+                m.mouseEntered(mouseEvent);
+            }
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            for (MouseListener m : getParent().getMouseListeners()) {
+                m.mouseExited(mouseEvent);
+            }
+        }
+    }
+
+    GUIAppearanceDescriptionPanel() {
+        setBackground(null);
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        document = new HTMLDocument();
+        text = new JTextPane(document);
+        text.setContentType("text/html");
+
+        ((DefaultCaret) text.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        text.setForeground(GUIColors.textColorLight);
+        text.setEditable(false);
+        text.setBackground(null);
+
+        // Pass MouseEvents to the parent
+        text.setHighlighter(null);
+        text.addMouseListener(new AppearanceDescriptionMouseListener());
+
+        add(text);
+    }
+
+    void setCharacter(NPC character) {
+        text.setPreferredSize(new Dimension(getParent().getWidth(), getParent().getHeight()));
+        StringBuilder buf = new StringBuilder();
+        character.body.describe(buf, Global.getPlayer(), " ");
+        System.out.println(buf.toString());
+        text.setText("<font face='Georgia' color='white' size='4'>" + buf.toString()
+            + "</font>");
+        System.out.println(document.getLength());
+    }
+}
--- NightgamesMod/nightgames/gui/GUIAppearancePanel.java
+++ NightgamesMod/nightgames/gui/GUIAppearancePanel.java
@@ -1,17 +1,48 @@
 package nightgames.gui;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 import nightgames.characters.NPC;
 import nightgames.combat.Combat;
 
 class GUIAppearancePanel extends JPanel {
 
+    private class AppearancePanelMouseListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+            toggle();
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+        }
+    }
+
     private GUIPortraitPanel portraitPanel;
+    private GUIAppearanceDescriptionPanel appearanceDescriptionPanel;
 
     GUIAppearancePanel() {
         portraitPanel = new GUIPortraitPanel();
+        appearanceDescriptionPanel = new GUIAppearanceDescriptionPanel();
         setBackground(null);
         add(portraitPanel);
+        add(appearanceDescriptionPanel);
+        appearanceDescriptionPanel.setVisible(false);
+        addMouseListener(new AppearancePanelMouseListener());
     }
 
     void clearPortrait() {
@@ -20,5 +51,11 @@ class GUIAppearancePanel extends JPanel {
 
     void loadPortrait(Combat c, NPC enemy) {
         portraitPanel.loadPortrait(c, enemy);
+        appearanceDescriptionPanel.setCharacter(enemy);
+    }
+
+    private void toggle() {
+        portraitPanel.setVisible(!portraitPanel.isVisible());
+        appearanceDescriptionPanel.setVisible(!appearanceDescriptionPanel.isVisible());
     }
 }
