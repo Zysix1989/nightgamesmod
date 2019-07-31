435149bc1c94540b0f2f48538dbef24022a2aaad
inproto451 <in.proto451@gmail.com>
2019-07-30 23:13:59 -0400

    Rewrite DivineMod to use templates

--- NightgamesMod/nightgames/characters/body/mods/DivineMod.java
+++ NightgamesMod/nightgames/characters/body/mods/DivineMod.java
@@ -10,6 +10,8 @@ import nightgames.status.DivineCharge;
 import nightgames.status.Stsflag;
 import nightgames.status.addiction.Addiction;
 import nightgames.status.addiction.AddictionType;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 public class DivineMod extends PartMod {
     public static final DivineMod INSTANCE = new DivineMod();
@@ -28,11 +30,21 @@ public class DivineMod extends PartMod {
         return "holy";
     }
 
-    public double applyBonuses(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, double damage) { 
+    public double applyBonuses(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, double damage) {
         if (self.getStatus(Stsflag.divinecharge) != null) {
-            c.write(self, Global.format(
-                            "{self:NAME-POSSESSIVE} concentrated divine energy in {self:possessive} %s seeps into {other:name-possessive} %s, sending unimaginable pleasure directly into {other:possessive} soul.",
-                            self, opponent, part.getType(), target.getType()));
+            JtwigModel model = JtwigModel.newModel()
+            .with("self", self)
+            .with("opponent", opponent)
+            .with("part", part)
+            .with("target", target);
+            JtwigTemplate template = JtwigTemplate.inlineTemplate( 
+                "{{ self.nameOrPossessivePronoun() }} concentrated divine energy in " 
+                    + "{{ self.possessiveAdjective() }} {{ part.getType() }} " 
+                    + "seeps into {{ opponent.nameOrPossessivePronoun() }} {{ target.getType() }}, "
+                    + "sending unimaginable pleasure directly into " 
+                    + "{{ opponent.possessiveAdjective() }} soul."
+                );
+            c.write(self, template.render(model));
         }
         // no need for any effects, the bonus is in the pleasure mod
         return 0;
@@ -40,46 +52,75 @@ public class DivineMod extends PartMod {
 
     public double applyReceiveBonuses(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, double damage) {
         DivineCharge charge = (DivineCharge) self.getStatus(Stsflag.divinecharge);
+        JtwigModel model = JtwigModel.newModel()
+            .with("self", self)
+            .with("opponent", opponent)
+            .with("part", part)
+            .with("target", target);
+        JtwigTemplate template;
         if (charge == null) {
-            c.write(self, Global.format(
-                            "{self:NAME-POSSESSIVE} " + part.fullDescribe(self)
-                                            + " radiates a golden glow when {self:pronoun-action:moan|moans}. "
-                                            + "%s feeding on {self:possessive} own pleasure to charge up {self:possessive} divine energy.",
-                            self, opponent, self == opponent ? "{self:SUBJECT-ACTION:are|is}" 
-                                            : "{other:SUBJECT-ACTION:realize|realizes} {self:subject-action:are|is}"));
+            template = JtwigTemplate.inlineTemplate(
+                "{{ self.nameOrPossessivePronoun() }} {{ part.fullDescribe(self) }} " 
+                    + "radiates a golden glow when {{ self.pronoun() }} {{ self.action('moan') }}. "
+                    + "{% if (self != opponent) %} "
+                    + "{{ opponent.pronoun() }} {{ opponent.action('realize') }} "
+                    + "{% endif %}"
+                    + "{{ self.pronoun() }} {{ self.action('are', 'is' }} " 
+                    + "feeding on {{ self.possessiveAdjective() }} own pleasure to charge up " 
+                    + "{{ self.possessiveAdjective() }} divine energy.");
             self.add(c, new DivineCharge(self, .25));
         } else {
-            c.write(self, Global.format(
-                            "{self:SUBJECT-ACTION:continue|continues} feeding on {self:possessive} own pleasure to charge up {self:possessive} divine energy.",
-                            self, opponent));
+            template = JtwigTemplate.inlineTemplate(
+                "{{ self.subject() }} {{ self.action('continue') }} feeding on " 
+                    + "{{ self.possessiveAdjective() }} own pleasure to charge up " 
+                    + "{{ self.possessiveAdjective() }} divine energy.");
             self.add(c, new DivineCharge(self, charge.magnitude));
         }
+        c.write(self, template.render(model));
         return 0;
     }
 
     @Override
     public void receiveCum(Combat c, Character self, BodyPart part, Character opponent, BodyPart target) {
         if (self.has(Trait.zealinspiring) && Global.random(4) > -10) {
+            
             if (c.getStance().partsForStanceOnly(c, self, opponent).contains(part) && c.getStance().partsForStanceOnly(c, opponent, self).stream().anyMatch(otherPart -> otherPart.isType("cock"))) {
-                c.write(self, Global.format(
-                                "As {other:possessive} cum floods {self:name-possessive} "
-                                                + "%s, {other:subject-action:are} assaulted by a feeling of inner peace and belonging. The soothing"
-                                                + " emotions washes over {other:pronoun}, before settling in as an unadulterated adoration for {other:possessive} divine partner.",
-                                self, opponent, part.describe(self)));
+                JtwigModel model = JtwigModel.newModel()
+                    .with("self", self)
+                    .with("opponent", opponent)
+                    .with("part", part)
+                    .with("target", target);
+                JtwigTemplate template = JtwigTemplate.inlineTemplate(
+                    "As {{ opponent.possessiveAdjective() }} cum floods "
+                        + "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }}, " 
+                        + "{opponent:subject-action:are} assaulted by a feeling of inner peace "
+                        + "and belonging. The soothing emotions washes over {{ opponent.pronoun() }}, "
+                        + "before settling in as an unadulterated adoration for " 
+                        + "{{ opponent.possessiveAdjective() }} divine partner.");
+                c.write(self, template.render(model));
                 opponent.addict(c, AddictionType.ZEAL, self, Addiction.MED_INCREASE);
             }
         }
     }
 
     public void onStartPenetration(Combat c, Character self, Character opponent, BodyPart part, BodyPart target) {
-        if (opponent.human()) {
-            c.write(self, Global.format(
-                            "<b>As soon as you penetrate {self:name-do}, you realize it was a bad idea. While it looks innocuous enough, {self:possessive} %s "
-                                            + "feels like pure ecstasy. You're not sure why you thought fucking a bonafide sex goddess was a good idea. "
-                                            + "{self:SUBJECT} isn't even moving yet, but warm walls of flesh knead your cock ceaselessly while her perfectly trained %s muscles constrict and "
-                                            + "relax around your dick, bringing you waves of pleasure.</b>",
-                            self, opponent, part.getType(), part.adjective()));
-        }
+        JtwigModel model = JtwigModel.newModel()
+                .with("self", self)
+                .with("opponent", opponent)
+                .with("part", part)
+                .with("target", target)
+                .with("dom", c.getStance().dom(self));
+        JtwigTemplate template = JtwigTemplate.inlineTemplate(
+                "<b>As soon as you penetrate {{ self.nameDirectObject() }}, you realize it "
+                    + "was a bad idea. While it looks innocuous enough, "
+                    + "{{ self.possessiveAdjective() }} {{ part.getType() }} "
+                    + "feels like pure ecstasy. {{ opponent.nameOrPossessivePronoun }} not sure why "
+                    + "{{ opponent.subject() }} thought fucking a bonafide sex goddess was a good "
+                    + "idea. {{ self.subject() }} {{ self.action('are', 'is' }}n't even moving "
+                    + "yet, but {{ self.possessiveAdjective() }} {{ part.describe(self) }} massages "
+                    + "{{ opponent.possessiveAdjective() }} {{ target.getType() }}, bringing you "
+                    + "waves of pleasure.</b>");
+        c.write(self, template.render(model));
     }
 
     @Override
