7e438e5dba8c24d499f43c181cd724c9b8052a5d
inproto451 <in.proto451@gmail.com>
2019-07-31 08:01:19 -0400

    Rewrite ExtendedTongueMod to use templates

--- NightgamesMod/nightgames/characters/body/mods/ExtendedTonguedMod.java
+++ NightgamesMod/nightgames/characters/body/mods/ExtendedTonguedMod.java
@@ -7,6 +7,8 @@ import nightgames.combat.Combat;
 import nightgames.global.Global;
 import nightgames.status.CockBound;
 import nightgames.status.Stsflag;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 public class ExtendedTonguedMod extends PartMod {
     public static final PartMod INSTANCE = new ExtendedTonguedMod();
@@ -21,20 +23,39 @@ public class ExtendedTonguedMod extends PartMod {
     }
 
     public String getLongDescriptionOverride(Character self, BodyPart part, String previousDescription) {
-        if (!part.isType("mouth")) {
-            return previousDescription + Global.format(" Occasionally, a pink tongue slides out of %s and licks {self:possessive} second lips.", self, self, part.getType());
-        } else {
-            return previousDescription + Global.format(" When {self:pronoun-action:open} {self:pronoun} mouth, you see an unnaturally long tongue.", self, self, part.getType());
-        }
+        JtwigModel model = JtwigModel.newModel()
+            .with("self", self)
+            .with("part", part);
+        JtwigTemplate template = JtwigTemplate.inlineTemplate(
+            "{% if part.isType('mouth') %}"
+                + "When {{ self.pronoun() }} {{ self.action('open') }} "
+                + "{{ self.possessiveAdjective() }} mouth, you see an unnaturally long tongue."
+                + "{% else %}"
+                + "Occasionally, a pink tongue slides out of {{ self.possessiveAdjective }} "
+                + "{{ part.getType() }} and licks {{ self.possessiveAdjective() }} second lips."
+                + "{% endif %}"
+        );
+        return previousDescription + " " + template.render(model);
     }
 
     public double applyBonuses(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, double damage) { 
         if (c.getStance().isPartFuckingPartInserted(c, opponent, target, self, part) && part.isType("pussy")) {
             if (target.isType("cock") && !opponent.hasStatus(Stsflag.cockbound)) {
-                opponent.add(c, new CockBound(opponent, 5, self.nameOrPossessivePronoun() + " " + part.adjective() + "-tongue"));
-                c.write(self, self.nameOrPossessivePronoun() + " long sinuous " + part.adjective() + " tongue wraps around "
-                                + opponent.nameOrPossessivePronoun() + " " + target.describe(opponent)
-                                + ", preventing any escape.\n");
+                JtwigModel model = JtwigModel.newModel()
+                    .with("self", self)
+                    .with("opponent", opponent)
+                    .with("part", part)
+                    .with("target", target);
+                JtwigTemplate bindingTemplate = JtwigTemplate.inlineTemplate(
+                    "{{ self.nameOrPossessivePronoun() }} {{ part.adjective() }}-tongue"
+                );
+                JtwigTemplate messageTemplate = JtwigTemplate.inlineTemplate(
+                    "{{ self.nameOrPossessivePronoun() }} long sinuous {{ part.adjective() }} "
+                        + "tongue wraps around {{ opponent.nameOrPossessivePronoun() }}"
+                        + "{{ target.describe(opponent) }}, preventing any escape."
+                );
+                opponent.add(c, new CockBound(opponent, 5,  bindingTemplate.render(model)));
+                c.write(self, messageTemplate.render(model));
             }
         }
         return 0;
