8dae5dda2f8c040ddfc15d0861bdb680d68ce596
inproto451 <in.proto451@gmail.com>
2019-07-31 20:40:17 -0400

    Rewrite ParasitedMod to use templates

--- NightgamesMod/nightgames/characters/body/mods/ParasitedMod.java
+++ NightgamesMod/nightgames/characters/body/mods/ParasitedMod.java
@@ -1,11 +1,11 @@
 package nightgames.characters.body.mods;
 
 import java.util.Optional;
-
 import nightgames.characters.Character;
 import nightgames.characters.body.BodyPart;
 import nightgames.combat.Combat;
-import nightgames.global.Global;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 public class ParasitedMod extends PartMod {
     public static final ParasitedMod INSTANCE = new ParasitedMod();
@@ -15,15 +15,27 @@ public class ParasitedMod extends PartMod {
     }
 
     public double applyReceiveBonuses(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, double damage) {
-        c.write(self, Global.format("The parasite inhabiting {self:name-possessive} %s is making {self:direct-object} <b>extremely sensitive</b>.", self, opponent, part.getType()));
+        JtwigModel model = JtwigModel.newModel()
+            .with("self", self)
+            .with("opponent", opponent)
+            .with("part", part)
+            .with("target", target);
+        JtwigTemplate template = JtwigTemplate.inlineTemplate(
+            "The parasite inhabiting {{ self.nameOrPossessivePronoun() }} {{ part.getType() }} "
+                + "is making {{ self.directObject() }} <b>extremely sensitive</b>.");
+        c.write(self, template.render(model));
         return 10;
     }
 
     public void onOrgasm(Combat c, Character self, Character opponent, BodyPart part) {
-        String partName = part.describe(self);
-        c.write(self, Global.format(
-                        "The force of {self:name-possessive} orgasm ejects the slimy lifeform from {self:possessive} %s.",
-                        self, opponent, partName));
+        JtwigModel model = JtwigModel.newModel()
+            .with("self", self)
+            .with("opponent", opponent)
+            .with("part", part);
+        JtwigTemplate template = JtwigTemplate.inlineTemplate(
+            "The force of {{ self.nameOrPossessivePronoun() }} orgasm ejects the slimy lifeform "
+                + "from {{ self.possessiveAdjective() }} {{ part.describe(self) }}.");
+        c.write(self, template.render(model));
         self.body.removeTemporaryPartMod(part.getType(), this);
     }
 
