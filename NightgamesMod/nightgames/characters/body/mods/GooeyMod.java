28aa5a7d7e4c5355d320a54060b3dc40df60259d
inproto451 <in.proto451@gmail.com>
2019-07-31 20:23:32 -0400

    Rewrite GooeyMod to use templates

--- NightgamesMod/nightgames/characters/body/mods/GooeyMod.java
+++ NightgamesMod/nightgames/characters/body/mods/GooeyMod.java
@@ -8,6 +8,8 @@ import nightgames.characters.body.BodyPart;
 import nightgames.combat.Combat;
 import nightgames.global.Global;
 import nightgames.status.CockBound;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 public class GooeyMod extends PartMod {
     public static final GooeyMod INSTANCE = new GooeyMod();
@@ -18,34 +20,56 @@ public class GooeyMod extends PartMod {
 
     public void onOrgasmWith(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, boolean selfCame) {
         if (c.getStance().isPartFuckingPartInserted(c, opponent, target, self, part) && !selfCame) {
-            String partName = part.describe(self);
-            c.write(self, Global.format(
-                            "{self:NAME-POSSESSIVE} %s clenches down hard"
-                                            + " on {other:name-possessive} {other:body-part:cock}. The suction is so strong that the cum"
-                                            + " leaves the shaft in a constant flow rather than spurts. When {other:possessive} orgasm is"
-                                            + " over, {other:subject-action:are} much more drained of cum than usual.",
-                            self, opponent, partName));
+            JtwigModel model = JtwigModel.newModel()
+                .with("self", self)
+                .with("opponent", opponent)
+                .with("part", part)
+                .with("target", target);
+            JtwigTemplate template = JtwigTemplate.inlineTemplate(
+                "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }} clenches down hard"
+                    + " on {{ other.nameOrPossessivePronoun() }} {{ target.describe(opponent) }}. "
+                    + "The suction is so strong that the cum leaves "
+                    + "{{ opponent.possessiveAdjective() }} shaft in a constant flow rather than "
+                    + "spurts. When {{ other.possessiveAdjective() }} orgasm is over, "
+                    + "{{ other.subject() }}{{{ other.action('are') }} much more drained of cum "
+                    + "than usual.");
+            c.write(self, template.render(model));
             opponent.loseWillpower(c, 10 + Global.random(Math.min(20, self.get(Attribute.Bio))));
         }
     }
 
     public void onStartPenetration(Combat c, Character self, Character opponent, BodyPart part, BodyPart target) {
         if (c.getStance().isPartFuckingPartInserted(c, opponent, target, self, part)) {
-            String partName = part.describe(self);
-            c.write(self, Global.format("{self:NAME-POSSESSIVE} %s envelops"
-                            + " {other:possessive} {other:body-part:cock} in a sticky grip, making extraction more"
-                            + " difficult.", self, opponent, partName));
-            opponent.add(c, new CockBound(opponent, 7, self.nameOrPossessivePronoun() + " " + partName));
+            JtwigModel model = JtwigModel.newModel()
+                .with("self", self)
+                .with("opponent", opponent)
+                .with("part", part)
+                .with("target", target);
+            JtwigTemplate template = JtwigTemplate.inlineTemplate(
+                "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }} envelops"
+                + " {{ other.possessiveAdjective() }} {{ target.describe(opponent) }} in a "
+                    + "sticky grip, making pulling out more difficult.");
+            JtwigTemplate bindingTemplate = JtwigTemplate.inlineTemplate(
+                "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }}"
+            );
+            c.write(self, template.render(model));
+            opponent.add(c, new CockBound(opponent, 7, bindingTemplate.render(model)));
         }
     }
 
-    public void tickHolding(Combat c, Character self, Character opponent, BodyPart part, BodyPart otherOrgan) {
-        String partName = part.describe(self);
-        c.write(self, Global.format(
-                        "The slimy filaments inside {self:possessive} %s constantly massage"
-                                        + " {other:possessive} %s, filling every inch of it with pleasure.",
-                        self, opponent, partName, otherOrgan.describe(opponent)));
-        opponent.body.pleasure(self, part, otherOrgan, 1 + Global.random(7), c);
+    public void tickHolding(Combat c, Character self, Character opponent, BodyPart part, BodyPart target) {
+        JtwigModel model = JtwigModel.newModel()
+            .with("self", self)
+            .with("opponent", opponent)
+            .with("part", part)
+            .with("target", target);
+        JtwigTemplate template = JtwigTemplate.inlineTemplate(
+            "The slimy filaments inside {{ self.possessiveAdjective() }} {{ part.describe(self) }} "
+                + "constantly massage {{ other.possessiveAdjective() }} "
+                + "{{ target.describe(opponent) }}, filling every inch of it with pleasure."
+        );
+        c.write(self, template.render(model));
+        opponent.body.pleasure(self, part, target, 1 + Global.random(7), c);
     }
 
     public Optional<String> getFluids() {
