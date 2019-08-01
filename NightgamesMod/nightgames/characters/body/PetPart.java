e7e3f28619169218af49b58935170f24a3e310aa
inproto451 <in.proto451@gmail.com>
2019-07-31 21:26:10 -0400

    Remove dead code

--- NightgamesMod/nightgames/characters/body/PetPart.java
+++
@@ -1,8 +0,0 @@
-package nightgames.characters.body;
-
-public class PetPart extends GenericBodyPart {
-
-    public PetPart(String name) {
-        super(name, 0.0, 1.0, 0.0, false, "pet", "");
-    }
-}
