9ed112f46eaac1489e4dd85ebb60fab67bd9573b
inproto451 <in.proto451@gmail.com>
2019-07-31 20:40:45 -0400

    No need for this to be public right now.

--- NightgamesMod/nightgames/characters/body/mods/ParasitedMod.java
+++ NightgamesMod/nightgames/characters/body/mods/ParasitedMod.java
@@ -10,7 +10,7 @@ import org.jtwig.JtwigTemplate;
 public class ParasitedMod extends PartMod {
     public static final ParasitedMod INSTANCE = new ParasitedMod();
 
-    public ParasitedMod() {
+    private ParasitedMod() {
         super("parasited", 0, 0, 0, -1000);
     }
 
