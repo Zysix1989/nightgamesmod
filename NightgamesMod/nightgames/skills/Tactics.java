101def608cbb023c13b4e6480b0ce02d0e5369ae
inproto451 <in.proto451@gmail.com>
2019-08-01 22:29:38 -0400

    Rip out now unused tactic groups.

--- NightgamesMod/nightgames/skills/Tactics.java
+++ NightgamesMod/nightgames/skills/Tactics.java
@@ -3,29 +3,24 @@ package nightgames.skills;
 import java.awt.Color;
 
 public enum Tactics {
-    damage(TacticGroup.hurt, new Color(150, 0, 0)),
-    pleasure(TacticGroup.pleasure, Color.PINK),
-    fucking(TacticGroup.pleasure, new Color(255, 100, 200)),
-    positioning(TacticGroup.positioning, new Color(0, 100, 0)),
-    stripping(TacticGroup.positioning, new Color(0, 100, 0)),
-    recovery(TacticGroup.recovery, Color.WHITE),
-    calming(TacticGroup.recovery, Color.WHITE),
-    debuff(TacticGroup.manipulation, Color.CYAN),
-    summoning(TacticGroup.manipulation, Color.YELLOW),
-    misc(TacticGroup.misc, new Color(200, 200, 200));
+    damage(new Color(150, 0, 0)),
+    pleasure(Color.PINK),
+    fucking(new Color(255, 100, 200)),
+    positioning(new Color(0, 100, 0)),
+    stripping(new Color(0, 100, 0)),
+    recovery( Color.WHITE),
+    calming(Color.WHITE),
+    debuff(Color.CYAN),
+    summoning(Color.YELLOW),
+    misc(new Color(200, 200, 200));
 
     private final Color color;
-    private final TacticGroup group;
-    Tactics(TacticGroup group, Color color) {
+    Tactics(Color color) {
         this.color = color;
-        this.group = group;
     }
 
     public Color getColor() {
         return color;
     }
 
-    public TacticGroup getGroup() {
-        return group;
-    }
 }
