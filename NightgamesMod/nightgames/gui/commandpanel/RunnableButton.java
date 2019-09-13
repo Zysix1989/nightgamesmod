6ab69112ea9f16f41efd2d7f4f65fe6f1d1c9d3b
inproto451 <in.proto451@gmail.com>
2019-09-13 19:11:29 -0400

    Use defined constants

--- NightgamesMod/nightgames/gui/commandpanel/RunnableButton.java
+++ NightgamesMod/nightgames/gui/commandpanel/RunnableButton.java
@@ -18,9 +18,9 @@ class RunnableButton extends KeyableButton {
 
     private void resetFontSize() {
         if (getButton().getText().contains("<br/>")) {
-            getButton().setFont(new Font("Baskerville Old Face", 0, 14));
+            getButton().setFont(new Font("Baskerville Old Face", Font.PLAIN, 14));
         } else {
-            getButton().setFont(new Font("Baskerville Old Face", 0, 18));            
+            getButton().setFont(new Font("Baskerville Old Face", Font.PLAIN, 18));
         }
     }
 
