c7be0795d48812ded7209299c4b1e0ba54af44d2
inproto451 <in.proto451@gmail.com>
2019-09-16 23:57:26 -0400

    Reduce visibility as much as possible

--- NightgamesMod/nightgames/items/BodyModEffect.java
+++ NightgamesMod/nightgames/items/BodyModEffect.java
@@ -4,13 +4,13 @@ import nightgames.characters.body.BodyPart;
 
 abstract class BodyModEffect extends ItemEffect {
     protected BodyPart affected;
-    protected int selfDuration;
+    int selfDuration;
 
-    public BodyModEffect(String selfVerb, String otherVerb, BodyPart affected) {
+    BodyModEffect(String selfVerb, String otherVerb, BodyPart affected) {
         this(selfVerb, otherVerb, affected, -1);
     }
 
-    public BodyModEffect(String selfVerb, String otherVerb, BodyPart affected, int duration) {
+    BodyModEffect(String selfVerb, String otherVerb, BodyPart affected, int duration) {
         super(selfVerb, otherVerb, true, true);
         this.affected = affected;
         selfDuration = duration;
