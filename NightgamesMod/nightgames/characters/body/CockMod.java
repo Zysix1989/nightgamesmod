000a81af8106c138fc9ab8ead8dad0f7daae5654
inproto451 <in.proto451@gmail.com>
2019-09-18 20:41:07 -0400

    Clean up duplicated stuff

--- NightgamesMod/nightgames/characters/body/CockMod.java
+++ NightgamesMod/nightgames/characters/body/CockMod.java
@@ -67,72 +67,7 @@ public class CockMod extends PartMod {
     @Override
     public double applyBonuses(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, double damage) {
         double bonus = super.applyBonuses(c, self, opponent, part, target, damage);
-        if (this.equals(blessed) && target.isType("cock")) {
-            if (self.getStatus(Stsflag.divinecharge) != null) {
-                c.write(self, Global.format(
-                                "{self:NAME-POSSESSIVE} concentrated divine energy in {self:possessive} cock rams into {other:name-possessive} pussy, sending unimaginable pleasure directly into {other:possessive} soul.",
-                                self, opponent));
-            }
-            // no need for any effects, the bonus is in the pleasure mod
-        }
-        if (this.equals(runic)) {
-            String message = "";
-            if (target.moddedPartCountsAs(opponent, DemonicMod.INSTANCE)) {
-                message += String.format(
-                                "The fae energies inside %s %s radiate outward and into %s, causing %s %s to grow much more sensitive. ",
-                                self.nameOrPossessivePronoun(), part.describe(self), opponent.nameOrPossessivePronoun(),
-                                opponent.possessiveAdjective(), target.describe(opponent));
-                bonus += damage * 0.5; // +50% damage
-            }
-            if (Global.random(8) == 0 && !opponent.wary()) {
-                message += String.format("Power radiates out from %s %s, seeping into %s and subverting %s will. ",
-                                self.nameOrPossessivePronoun(), part.describe(self), opponent.nameOrPossessivePronoun(),
-                                opponent.directObject());
-                opponent.add(c, new Enthralled(opponent, self, 3));
-            }
-            if (self.hasStatus(Stsflag.cockbound)) {
-                String binding = ((CockBound) self.getStatus(Stsflag.cockbound)).binding;
-                message += String.format(
-                                "With the merest of thoughts, %s %s out a pulse of energy from %s %s, freeing it from %s %s. ",
-                                self.subject(), self.human() ? "send" : "sends", self.possessiveAdjective(),
-                                part.describe(self), opponent.nameOrPossessivePronoun(), binding);
-                self.removeStatus(Stsflag.cockbound);
-            }
-            c.write(self, message);
-        } else if (this.equals(incubus)) {
-            String message = String.format("%s demonic appendage latches onto %s will, trying to draw it into %s.",
-                            self.nameOrPossessivePronoun(), opponent.nameOrPossessivePronoun(),
-                            self.reflectivePronoun());
-            int amtDrained;
-            if (target.moddedPartCountsAs(opponent, FeralMod.INSTANCE)) {
-                message += String.format(" %s %s gladly gives it up, eager for more pleasure.",
-                                opponent.possessiveAdjective(), target.describe(opponent));
-                amtDrained = 5;
-                bonus += 2;
-            } else if (target.moddedPartCountsAs(opponent, CyberneticMod.INSTANCE)) {
-                message += String.format(
-                                " %s %s does not oblige, instead sending a pulse of electricity through %s %s and up %s spine",
-                                opponent.nameOrPossessivePronoun(), target.describe(opponent),
-                                self.nameOrPossessivePronoun(), part.describe(self), self.possessiveAdjective());
-                self.pain(c, opponent, Global.random(9) + 4);
-                amtDrained = 0;
-            } else {
-                message += String.format(" Despite %s best efforts, some of the elusive energy passes into %s.",
-                                opponent.nameOrPossessivePronoun(), self.nameDirectObject());
-                amtDrained = 3;
-            }
-            int strength = (int) self.modifyDamage(DamageType.drain, opponent, amtDrained);
-        	if (amtDrained != 0) {
-        		if (self.isPet()) {
-	                Character master = ((PetCharacter) self).getSelf().owner();
-	                c.write(self, Global.format("The stolen strength seems to flow through to {self:possessive} {other:master} through {self:possessive} infernal connection.", self, master));
-	                opponent.drainWillpower(c, master, strength);
-	            } else {
-	                opponent.drainWillpower(c, self, strength);
-                }
-            }
-            c.write(self, message);
-        } else if (this.equals(bionic)) {
+        if (this.equals(bionic)) {
             String message = "";
             if (Global.random(5) == 0 && target.getType().equals("pussy")) {
                 message += String.format(
@@ -204,63 +139,11 @@ public class CockMod extends PartMod {
     }
 
     public double applyReceiveBonuses(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, double damage) {
-        if (this.equals(blessed) && c.getStance().inserted(self)) {
-            DivineCharge charge = (DivineCharge) self.getStatus(Stsflag.divinecharge);
-            if (charge == null) {
-                c.write(self, Global.format(
-                                "{self:NAME-POSSESSIVE} " + part.fullDescribe(self)
-                                                + " radiates a golden glow as {self:subject-action:groan|groans}. "
-                                                + "{other:SUBJECT-ACTION:realize|realizes} {self:subject-action:are|is} feeding on {self:possessive} own pleasure to charge up {self:possessive} divine energy.",
-                                self, opponent));
-                self.add(c, new DivineCharge(self, .25));
-            } else {
-                c.write(self, Global.format(
-                                "{self:SUBJECT-ACTION:continue|continues} feeding on {self:possessive} own pleasure to charge up {self:possessive} divine energy.",
-                                self, opponent));
-                self.add(c, new DivineCharge(self, charge.magnitude));
-            }
-        }
         return 0;
     }
 
     @Override
     public void onOrgasmWith(Combat c, Character self, Character opponent, BodyPart part, BodyPart target, boolean selfCame) {
-        if (this.equals(incubus) && c.getStance().inserted(self)) {
-            if (selfCame) {
-                if (target.moddedPartCountsAs(opponent, CyberneticMod.INSTANCE)) {
-                    c.write(self, String.format(
-                                    "%s demonic seed splashes pointlessly against the walls of %s %s, failing even in %s moment of defeat.",
-                                    self.nameOrPossessivePronoun(), opponent.nameOrPossessivePronoun(),
-                                    target.describe(opponent), self.possessiveAdjective()));
-                } else {
-                    int duration = Global.random(3) + 2;
-                    String message = String.format(
-                                    "The moment %s erupts inside %s, %s mind goes completely blank, leaving %s pliant and ready.",
-                                    self.subject(), opponent.subject(), opponent.possessiveAdjective(),
-                                    opponent.directObject());
-                    if (target.moddedPartCountsAs(opponent, FeralMod.INSTANCE)) {
-                        message += String.format(" %s no resistance to the subversive seed.",
-                                        Global.capitalizeFirstLetter(opponent.subjectAction("offer", "offers")));
-                        duration += 2;
-                    }
-                    opponent.add(c, new Enthralled(opponent, self, duration));
-                    c.write(self, message);
-                }
-            } else {
-                if (!target.moddedPartCountsAs(opponent, CyberneticMod.INSTANCE)) {
-                    c.write(self, String.format(
-                                    "Sensing %s moment of passion, %s %s greedily draws upon the rampant flows of orgasmic energy within %s, transferring the power back into %s.",
-                                    opponent.nameOrPossessivePronoun(), self.nameOrPossessivePronoun(),
-                                    part.describe(self), opponent.directObject(), self.directObject()));
-                    int attDamage = target.moddedPartCountsAs(opponent, FeralMod.INSTANCE) ? 10 : 5;
-                    int willDamage = target.moddedPartCountsAs(opponent, FeralMod.INSTANCE) ? 10 : 5;
-                    Drained.drain(c, self, opponent, Attribute.Power, attDamage, 20, true);
-                    Drained.drain(c, self, opponent, Attribute.Cunning, attDamage, 20, true);
-                    Drained.drain(c, self, opponent, Attribute.Seduction, attDamage, 20, true);
-                    opponent.drainWillpower(c, self, (int) self.modifyDamage(DamageType.drain, opponent, willDamage));
-                }
-            }
-        }
     }
 
     @Override
@@ -276,14 +159,6 @@ public class CockMod extends PartMod {
 
     @Override
     public void onStartPenetration(Combat c, Character self, Character opponent, BodyPart part, BodyPart target) {
-        if (this.equals(blessed) && target.isErogenous()) {
-            if (!self.human()) {
-                c.write(self, Global.format(
-                                "As soon as {self:subject} penetrates you, you realize you're screwed. Both literally and figuratively. While it looks innocuous enough, {self:name-possessive} {self:body-part:cock} "
-                                                + "feels like pure ecstasy. {self:SUBJECT} hasn't even begun moving yet, but {self:possessive} cock simply sitting within you radiates a heat that has you squirming uncontrollably.",
-                                self, opponent));
-            }
-        }
     }
 
     public void onEndPenetration(Combat c, Character self, Character opponent, BodyPart part, BodyPart target) {
@@ -297,16 +172,10 @@ public class CockMod extends PartMod {
     public String describeAdjective(String partType) {
         if (this.equals(bionic)) {
             return "bionic implants";
-        } else if (this.equals(blessed)) {
-            return "holy aura";
         } else if (this.equals(enlightened)) {
             return "imposing presence";
-        } else if (this.equals(runic)) {
-            return "runic symbols";
         } else if (this.equals(primal)) {
             return "primal musk";
-        } else if (this.equals(incubus)) {
-            return "corruption";
         }
         return "weirdness (ERROR)";
     }
