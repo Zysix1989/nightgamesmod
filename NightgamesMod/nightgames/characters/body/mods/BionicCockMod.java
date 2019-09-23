db0fef8c4353a49d68b0253beff1548aa7f4b8d1
inproto451 <in.proto451@gmail.com>
2019-09-22 23:40:02 -0400

    Migrate BionicCockMod to templates

--- NightgamesMod/nightgames/characters/body/mods/BionicCockMod.java
+++ NightgamesMod/nightgames/characters/body/mods/BionicCockMod.java
@@ -9,6 +9,8 @@ import nightgames.global.Global;
 import nightgames.status.FluidAddiction;
 import nightgames.status.Hypersensitive;
 import nightgames.status.Winded;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 public class BionicCockMod extends CockMod {
     public static final String TYPE = "bionic";
@@ -23,16 +25,23 @@ public class BionicCockMod extends CockMod {
         double bonus = super.applyBonuses(c, self, opponent, part, target, damage);
         String message = "";
         if (Global.random(5) == 0 && target.getType().equals("pussy")) {
-            message += String.format(
-                "%s %s out inside %s %s, pressing the metallic head of %s %s tightly against %s cervix. "
-                    + "Then, a thin tube extends from %s uthera and into %s womb, pumping in a powerful aphrodisiac that soon has %s sensitive and"
-                    + " gasping for more.",
-                self.subject(), self.human() ? "bottom" : "bottoms",
-                opponent.nameOrPossessivePronoun(),
-                target.describe(opponent), self.possessiveAdjective(), part.describe(self),
-                opponent.possessiveAdjective(), self.possessiveAdjective(),
-                opponent.possessiveAdjective(),
-                opponent.objectPronoun());
+            var model = JtwigModel.newModel()
+                .with("self", self)
+                .with("opponent", opponent)
+                .with("part", part)
+                .with("target", target);
+            var template = JtwigTemplate.inlineTemplate(
+                "{{ self.subject() }} {{ self.action('bottom') }} out inside "
+                    + "{{ opponent.nameOrPossessivePronoun() }} {{ target.describe(opponent) }}, "
+                    + "pressing the metallic head of "
+                    + "{{ self.possessiveAdjective() }} {{ part.describe(self) }} tightly against "
+                    + "{{ opponent.possessiveAdjective() }} cervix. "
+                    + "Then, a thin tube extends from {{ self.possessiveAdjective() }} urethra and "
+                    + "into {{ opponent.possessiveAdjective() }} womb, pumping in a powerful "
+                    + "aphrodisiac that soon has {{ opponent.objectPronoun() }} sensitive and"
+                    + " gasping for more."
+            );
+            message += template.render(model);
             opponent.add(c, new Hypersensitive(opponent));
             // Instantly addict
             opponent.add(c, new FluidAddiction(opponent, self, 1, 2));
@@ -40,16 +49,25 @@ public class BionicCockMod extends CockMod {
             opponent.add(c, new FluidAddiction(opponent, self, 1, 2));
             bonus -= 3; // Didn't actually move around too much
         } else if (target.moddedPartCountsAs(FieryMod.TYPE)) {
-            message += String.format(
-                "Sensing the flesh around it, %s %s starts spinning rapidly, vastly increasing the friction against the walls of %s %s.",
-                self.nameOrPossessivePronoun(), part.describe(self),
-                opponent.nameOrPossessivePronoun(),
-                target.describe(opponent));
+            var model = JtwigModel.newModel()
+                .with("self", self)
+                .with("opponent", opponent)
+                .with("part", part)
+                .with("target", target);
+            var template = JtwigTemplate.inlineTemplate(
+                "Sensing the flesh around it, {{ self.nameOrPossessivePronoun }} "
+                    + "{{ part.describe(self) }} starts spinning rapidly, vastly increasing the "
+                    + "friction against the walls of {{ opponent.nameOrPossessivePronoun() }} "
+                    + "{{ target.describe(opponent) }}."
+            );
+            message += template.render(model);
             bonus += 5;
             if (Global.random(5) == 0) {
-                message += String.format(
-                    " The intense sensations cause %s to forget to breathe for a moment, leaving %s literally breathless.",
-                    opponent.subject(), opponent.objectPronoun());
+                var template2 = JtwigTemplate.inlineTemplate(
+                    "The intense sensations cause {{ opponent.subject() }} to forget to breathe "
+                        + "for a moment, leaving {{ opponent.objectPronoun() }} literally breathless."
+                );
+                message += " " + template2.render(model);
                 opponent.add(c, new Winded(opponent, 1));
             }
         }
