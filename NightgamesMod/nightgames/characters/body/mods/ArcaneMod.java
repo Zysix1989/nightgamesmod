357ba6f03e8560408f68493765b620375ace7b7f
inproto451 <in.proto451@gmail.com>
2019-09-23 20:28:44 -0400

    Extract Arcane templates to vars for performance and readability

--- NightgamesMod/nightgames/characters/body/mods/ArcaneMod.java
+++ NightgamesMod/nightgames/characters/body/mods/ArcaneMod.java
@@ -32,55 +32,27 @@ public class ArcaneMod extends PartMod {
         ArrayList<JtwigTemplate> templates = new ArrayList<>();
         if (!target.moddedPartCountsAs(BionicCockMod.TYPE)) {
             if (target.moddedPartCountsAs(PrimalCockMod.TYPE)) {
-                templates.add(JtwigTemplate.inlineTemplate(
-                    " {{ if part.isType('mouth') -}} The arcane lipstick painted on "
-                        + "{{ else - }}The tattoos around {{ endif -}}"
-                        + "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }} flare up "
-                        + "with a new intensity, responding to the energy flow from "
-                        + "{{ opponent.nameOrPossessivePronoun() }} {{ target.describe(opponent) }}. "
-                        + "The magic within them latches onto it and pulls fiercly, drawing "
-                        + "{{ opponent.possessiveAdjective() }} strength into "
-                        + "{{ opponent.possessiveAdjective() }} with great gulps."));
+                templates.add(APPLY_BONUS_PRIMAL_TEMPLATE);
                 strength = 10 + self.get(Attribute.Arcane) / 4;
             } else {
                 model.with("fucking", c.getStance()
                     .isPartFuckingPartInserted(c, opponent, target, self, part));
-                templates.add(JtwigTemplate.inlineTemplate(
-                    "{{ self.nameOrPossessivePronoun() }} "
-                        + "{{ (part.isType('mouth')) ? 'lipstick on' : 'tattoos surrounding' }} "
-                        + "{{ self.possessiveAdjective() }} {{ part.getType() }} light up with "
-                        + "arcane energy as "
-                        + "{% if (fucking) %} {{ opponent.subjectAction('are', 'is') }} inside "
-                        + "{{ self.objectPronoun() }}"
-                        + "{% else %}{{ self.subjectAction('touch') }} {{ opponent.objectPronoun() }}"
-                        + "{% endif %} channeling some of {{ opponent.possessiveAdjective() }} "
-                        + "energies back to its master."));
+                templates.add(APPLY_BONUS_TEMPLATE);
                 strength = 5 + self.get(Attribute.Arcane) / 6;
             }
             opponent.drainMojo(c, self, strength);
             if (self.isPet()) {
                 Character master = ((PetCharacter) self).getSelf().owner();
                 model.with("master", master);
-                templates.add(JtwigTemplate.inlineTemplate(
-                    "The energy seems to flow through {{ self.objectPronoun() }} and into "
-                        + "{{ self.possessiveAdjective() }} "
-                        + "{{ (master.useFemalePronouns()) ? 'mistress' : 'master' }}." ));
+                templates.add(APPLY_BONUS_PET_TEMPLATE);
                 master.buildMojo(c, strength);
             }
             if (Global.random(8) == 0 && !opponent.wary()) {
-                templates.add(JtwigTemplate.inlineTemplate(
-                    "The light seems to seep into {{ opponent.possessiveAdjective() }} "
-                        + " {{ target.describe(opponent) }} leaving {{ opponent.objectPronoun() }} "
-                        + "enthralled to {{ self.possessiveAdjective() }} will."));
+                templates.add(APPLY_BONUS_ENTHRALL_TEMPLATE);
                 opponent.add(c, new Enthralled(opponent, self, 3));
             }
         } else {
-            templates.add(JtwigTemplate.inlineTemplate(
-                "{{ self.nameOrPossessivePronoun() }} "
-                    + "{{ (part.isType('mouth')) ? \"lipstick\" : \"tattoos\" }} shine with an "
-                    + "eldritch light, but they do not seem to be able to affect "
-                    + "{{ opponent.nameOrPossessivePronoun() }} only partially-organic "
-                    + "{{ target.describe(opponent) }}"));
+            templates.add(APPLY_BONUS_BIONIC_TEMPLATE);
         }
         for (JtwigTemplate template : templates) {
             c.write(self, template.render(model));
@@ -100,4 +72,42 @@ public class ArcaneMod extends PartMod {
         }
         return "arcane tattoos";
     }
+
+    private static final JtwigTemplate APPLY_BONUS_PRIMAL_TEMPLATE = JtwigTemplate.inlineTemplate(
+        " {{ if part.isType('mouth') -}} The arcane lipstick painted on "
+            + "{{ else - }}The tattoos around {{ endif -}}"
+            + "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }} flare up "
+            + "with a new intensity, responding to the energy flow from "
+            + "{{ opponent.nameOrPossessivePronoun() }} {{ target.describe(opponent) }}. "
+            + "The magic within them latches onto it and pulls fiercly, drawing "
+            + "{{ opponent.possessiveAdjective() }} strength into "
+            + "{{ opponent.possessiveAdjective() }} with great gulps.");
+
+    private static final JtwigTemplate APPLY_BONUS_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "{{ self.nameOrPossessivePronoun() }} "
+            + "{{ (part.isType('mouth')) ? 'lipstick on' : 'tattoos surrounding' }} "
+            + "{{ self.possessiveAdjective() }} {{ part.getType() }} light up with "
+            + "arcane energy as "
+            + "{% if (fucking) %} {{ opponent.subjectAction('are', 'is') }} inside "
+            + "{{ self.objectPronoun() }}"
+            + "{% else %}{{ self.subjectAction('touch') }} {{ opponent.objectPronoun() }}"
+            + "{% endif %} channeling some of {{ opponent.possessiveAdjective() }} "
+            + "energies back to its master.");
+
+    private static final JtwigTemplate APPLY_BONUS_PET_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "The energy seems to flow through {{ self.objectPronoun() }} and into "
+            + "{{ self.possessiveAdjective() }} "
+            + "{{ (master.useFemalePronouns()) ? 'mistress' : 'master' }}." );
+
+    private static final JtwigTemplate APPLY_BONUS_ENTHRALL_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "The light seems to seep into {{ opponent.possessiveAdjective() }} "
+            + " {{ target.describe(opponent) }} leaving {{ opponent.objectPronoun() }} "
+            + "enthralled to {{ self.possessiveAdjective() }} will.");
+
+    private static final JtwigTemplate APPLY_BONUS_BIONIC_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "{{ self.nameOrPossessivePronoun() }} "
+            + "{{ (part.isType('mouth')) ? \"lipstick\" : \"tattoos\" }} shine with an "
+            + "eldritch light, but they do not seem to be able to affect "
+            + "{{ opponent.nameOrPossessivePronoun() }} only partially-organic "
+            + "{{ target.describe(opponent) }}");
 }
