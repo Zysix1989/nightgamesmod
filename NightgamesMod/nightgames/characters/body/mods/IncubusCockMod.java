f51808e43dc5c72092c9a66cea17f66bc9cf5e16
inproto451 <in.proto451@gmail.com>
2019-09-24 21:58:39 -0400

    Extract IncubusCockMod templates for readability and performance

--- NightgamesMod/nightgames/characters/body/mods/IncubusCockMod.java
+++ NightgamesMod/nightgames/characters/body/mods/IncubusCockMod.java
@@ -15,6 +15,9 @@ import org.jtwig.JtwigTemplate;
 
 public class IncubusCockMod extends CockMod {
     public static final String TYPE = "incubus";
+
+
+
     public IncubusCockMod() {
         super(TYPE, 1.25, 1.3, .9);
     }
@@ -25,37 +28,18 @@ public class IncubusCockMod extends CockMod {
         var model = JtwigModel.newModel()
             .with("self", self)
             .with("opponent", opponent);
-        var template = JtwigTemplate.inlineTemplate(
-            "{{ self.nameOrPossessivePronoun() }} demonic appendage latches onto "
-                + "{{ opponent.nameOrPossessivePronoun() }} will, trying to draw it "
-                + "into {{ self.reflexivePronoun() }}."
-        );
-        String message = template.render(model);
+        String message = APPLY_BONUS_TEMPLATE.render(model);
         int amtDrained;
         if (target.moddedPartCountsAs(FeralMod.TYPE)) {
-            var template2 = JtwigTemplate.inlineTemplate(
-                "{{ opponent.possessiveAdjective() }} {{ target.describe(opponent) }} gladly "
-                    + "gives it up, eager for more pleasure."
-            );
-            message += " " + template2.render(model);
+            message += " " + APPLY_BONUS_FERAL_TEMPLATE.render(model);
             amtDrained = 5;
             bonus += 2;
         } else if (target.moddedPartCountsAs(CyberneticMod.TYPE)) {
-            var template2 = JtwigTemplate.inlineTemplate(
-                "{{ opponent.nameOrPossessivePronoun() }} {{ target.describe(opponent) }} does "
-                    + "not oblige, instead sending a pulse of electricity through "
-                    + "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }} and up "
-                    + "{{ self.possessiveAdjective() }} spine."
-            );
-            message += template2.render(model);
+            message += APPLY_BONUS_CYBERNETIC_TEMPLATE.render(model);
             self.pain(c, opponent, Global.random(9) + 4);
             amtDrained = 0;
         } else {
-            var template2 = JtwigTemplate.inlineTemplate(
-                "Despite {{ opponent.nameOrPossessivePronoun() }} best efforts, some of the "
-                    + "elusive energy passes into {{ self.nameDirectObject() }}."
-            );
-            message += template2.render(model);
+            message += APPLY_BONUS_2_TEMPLATE.render(model);
             amtDrained = 3;
         }
         int strength = (int) self.modifyDamage(DamageType.drain, opponent, amtDrained);
@@ -63,11 +47,7 @@ public class IncubusCockMod extends CockMod {
             if (self.isPet()) {
                 Character master = ((PetCharacter) self).getSelf().owner();
                 model.with("master", master);
-                var template2 = JtwigTemplate.inlineTemplate(
-                    "The stolen strength seems to flow through to {{ self.possessiveAdjective }} "
-                        + "{{ master.masterOrMistress() }} through their infernal connection."
-                );
-                c.write(self, template2.render(model));
+                c.write(self, APPLY_BONUS_PET_TEMPLATE.render(model));
                 opponent.drainWillpower(c, master, strength);
             } else {
                 opponent.drainWillpower(c, self, strength);
@@ -87,22 +67,10 @@ public class IncubusCockMod extends CockMod {
                 .with("target", target);
             if (selfCame) {
                 if (target.moddedPartCountsAs(CyberneticMod.TYPE)) {
-                    var template = JtwigTemplate.inlineTemplate(
-                        "{{ self.nameOrPossessivePronoun() }} demonic seed splashes pointlessly "
-                            + "against the walls of {{ opponent.nameOrPossessivePronoun() }} "
-                            + "{{ target.describe(opponent) }}, failing even in "
-                            + " {{ self.possessiveAdjective() }} moment of defeat."
-                    );
-                    c.write(self, template.render(model));
+                    c.write(self, ON_ORGASM_WITH_CYBERNETIC_TEMPLATE.render(model));
                 } else {
                     int duration = Global.random(3) + 2;
-                    var template = JtwigTemplate.inlineTemplate(
-                        "The moment {{ self.subject }} {{ self.action('erupt') }} inside "
-                            + "{{ opponent.subject() }}, {{ opponent.possessiveAdjective}} mind "
-                            + "goes completely blank, leaving {{ opponent.possessiveAdjective() }} "
-                            + "pliant and ready."
-                    );
-                    String message = template.render(model);
+                    String message = ON_ORGASM_WITH_FERAL_TEMPLATE.render(model);
                     if (target.moddedPartCountsAs(FeralMod.TYPE)) {
                         var template2 = JtwigTemplate.inlineTemplate("{{ opponent.subject() }} {{ opponent.action('offer') }} "
                             + "no resistance to the subversive seed."
@@ -115,14 +83,7 @@ public class IncubusCockMod extends CockMod {
                 }
             } else {
                 if (!target.moddedPartCountsAs(CyberneticMod.TYPE)) {
-                    var template = JtwigTemplate.inlineTemplate(
-                      "Sensing {{ opponent.nameOrPossessivePronoun() }} moment of passion, "
-                          + "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }} "
-                          + "greedily draws upon the rampant flows of orgasmic energy within "
-                          + "{{ opponent.objectPronoun() }} transferring the power back into "
-                          + "{{ self.objectPronoun() }}."
-                    );
-                    c.write(self, template.render(model));
+                    c.write(self, ON_ORGASM_WITH_TEMPLATE.render(model));
                     int attDamage = target.moddedPartCountsAs(FeralMod.TYPE) ? 10 : 5;
                     int willDamage = target.moddedPartCountsAs(FeralMod.TYPE) ? 10 : 5;
                     Drained.drain(c, self, opponent, Attribute.Power, attDamage, 20, true);
@@ -138,4 +99,54 @@ public class IncubusCockMod extends CockMod {
     public String describeAdjective(String partType) {
         return "corruption";
     }
+
+    private static final JtwigTemplate APPLY_BONUS_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "{{ self.nameOrPossessivePronoun() }} demonic appendage latches onto "
+            + "{{ opponent.nameOrPossessivePronoun() }} will, trying to draw it "
+            + "into {{ self.reflexivePronoun() }}."
+    );
+
+    private static final JtwigTemplate APPLY_BONUS_FERAL_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "{{ opponent.possessiveAdjective() }} {{ target.describe(opponent) }} gladly "
+            + "gives it up, eager for more pleasure."
+    );
+
+    private static final JtwigTemplate APPLY_BONUS_CYBERNETIC_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "{{ opponent.nameOrPossessivePronoun() }} {{ target.describe(opponent) }} does "
+            + "not oblige, instead sending a pulse of electricity through "
+            + "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }} and up "
+            + "{{ self.possessiveAdjective() }} spine."
+    );
+
+    private static final JtwigTemplate APPLY_BONUS_2_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "Despite {{ opponent.nameOrPossessivePronoun() }} best efforts, some of the "
+            + "elusive energy passes into {{ self.nameDirectObject() }}."
+    );
+
+    private static final JtwigTemplate APPLY_BONUS_PET_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "The stolen strength seems to flow through to {{ self.possessiveAdjective }} "
+            + "{{ master.masterOrMistress() }} through their infernal connection."
+    );
+
+    private static final JtwigTemplate ON_ORGASM_WITH_CYBERNETIC_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "{{ self.nameOrPossessivePronoun() }} demonic seed splashes pointlessly "
+            + "against the walls of {{ opponent.nameOrPossessivePronoun() }} "
+            + "{{ target.describe(opponent) }}, failing even in "
+            + " {{ self.possessiveAdjective() }} moment of defeat."
+    );
+
+    private static final JtwigTemplate ON_ORGASM_WITH_FERAL_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "The moment {{ self.subject }} {{ self.action('erupt') }} inside "
+            + "{{ opponent.subject() }}, {{ opponent.possessiveAdjective}} mind "
+            + "goes completely blank, leaving {{ opponent.possessiveAdjective() }} "
+            + "pliant and ready."
+    );
+
+    private static final JtwigTemplate ON_ORGASM_WITH_TEMPLATE = JtwigTemplate.inlineTemplate(
+        "Sensing {{ opponent.nameOrPossessivePronoun() }} moment of passion, "
+            + "{{ self.nameOrPossessivePronoun() }} {{ part.describe(self) }} "
+            + "greedily draws upon the rampant flows of orgasmic energy within "
+            + "{{ opponent.objectPronoun() }} transferring the power back into "
+            + "{{ self.objectPronoun() }}."
+    );
 }
