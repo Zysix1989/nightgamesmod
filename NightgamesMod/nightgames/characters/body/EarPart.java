e473863ea6aaf791c84738739a353d035e1d7db8
inproto451 <in.proto451@gmail.com>
2019-10-05 16:58:50 -0400

    Extract common code to base class

--- NightgamesMod/nightgames/characters/body/EarPart.java
+++ NightgamesMod/nightgames/characters/body/EarPart.java
@@ -1,6 +1,9 @@
 package nightgames.characters.body;
 
 import com.google.gson.JsonObject;
+import nightgames.characters.Character;
+import nightgames.combat.Combat;
+import nightgames.global.Global;
 
 public abstract class EarPart implements BodyPart {
     public String desc;
@@ -21,4 +24,124 @@ public abstract class EarPart implements BodyPart {
         throw new IllegalArgumentException("expected an enum field with one of the ear types");
     }
 
+    @Override
+    public String canonicalDescription() {
+        return desc + "ears";
+    }
+
+    @Override
+    public String fullDescribe(Character c) {
+        return desc + "ears";
+    }
+
+    @Override
+    public double priority(Character c) {
+        return getPleasure(c, null);
+    }
+
+    @Override
+    public double getHotness(Character self, Character opponent) {
+        return hotness;
+    }
+
+    @Override
+    public double getPleasure(Character self, BodyPart target) {
+        return pleasure;
+    }
+
+    @Override
+    public double getSensitivity(Character self, BodyPart target) {
+        return sensitivity;
+    }
+
+    @Override
+    public double applySubBonuses(Character self, Character opponent, BodyPart with, BodyPart target, double damage,
+        Combat c) {
+        return 0;
+    }
+
+    @Override
+    public int counterValue(BodyPart otherPart, Character self, Character other) {
+        return 0;
+    }
+
+    @Override
+    public boolean isVisible(Character c) {
+        return true;
+    }
+
+    @Override
+    public void describeLong(StringBuilder b, Character c) {
+        b.append(
+            Global.capitalizeFirstLetter(fullDescribe(c)) + " frames " + c.possessiveAdjective()
+                + " face.");
+    }
+
+    @Override
+    public String toString() {
+        return desc + "ears";
+    }
+
+    @Override
+    public boolean isType(String type) {
+        return type.equalsIgnoreCase("ears");
+    }
+
+    @Override
+    public String getType() {
+        return "ears";
+    }
+
+    @Override
+    public boolean isReady(Character self) {
+        return true;
+    }
+
+    @Override
+    public String getFluids(Character c) {
+        return "";
+    }
+
+    @Override
+    public boolean isErogenous() {
+        return false;
+    }
+
+    @Override
+    public BodyPart upgrade() {
+        return this;
+    }
+
+    @Override
+    public BodyPart downgrade() {
+        return this;
+    }
+
+    @Override
+    public void tickHolding(Combat c, Character self, Character opponent, BodyPart otherOrgan) { }
+
+    @Override
+    public int compare(BodyPart other) {
+        return 0;
+    }
+
+    @Override
+    public double applyBonuses(Character self, Character opponent, BodyPart target, double damage, Combat c) {
+        return 0;
+    }
+
+    @Override
+    public double applyReceiveBonuses(Character self, Character opponent, BodyPart target, double damage, Combat c) {
+        return 0;
+    }
+
+    @Override
+    public String prefix() {
+        return "";
+    }
+
+    @Override
+    public String adjective() {
+        return "otic";
+    }
 }
--- NightgamesMod/nightgames/characters/body/EarsPart.java
+++ NightgamesMod/nightgames/characters/body/EarsPart.java
@@ -5,8 +5,6 @@ import java.util.Collection;
 import java.util.Collections;
 import nightgames.characters.Attribute;
 import nightgames.characters.Character;
-import nightgames.combat.Combat;
-import nightgames.global.Global;
 
 public class EarsPart extends EarPart implements BodyPartMod {
     public static final String TYPE = "normal";
@@ -19,65 +17,8 @@ public class EarsPart extends EarPart implements BodyPartMod {
     }
 
     @Override
-    public String canonicalDescription() {
-        return desc + "ears";
-    }
-
-    @Override
-    public void describeLong(StringBuilder b, Character c) {
-        b.append(
-            Global.capitalizeFirstLetter(fullDescribe(c)) + " frames " + c.possessiveAdjective()
-                + " face.");
-    }
-
-    @Override
     public String describe(Character c) {
         return "ears";
-    }
-
-    @Override
-    public double priority(Character c) {
-        return getPleasure(c, null);
-    }
-
-    @Override
-    public String fullDescribe(Character c) {
-        return desc + "ears";
-    }
-
-    @Override
-    public String toString() {
-        return desc + "ears";
-    }
-
-    @Override
-    public boolean isType(String type) {
-        return type.equalsIgnoreCase("ears");
-    }
-
-    @Override
-    public String getType() {
-        return "ears";
-    }
-
-    @Override
-    public double getHotness(Character self, Character opponent) {
-        return hotness;
-    }
-
-    @Override
-    public double getPleasure(Character self, BodyPart target) {
-        return pleasure;
-    }
-
-    @Override
-    public double getSensitivity(Character self, BodyPart target) {
-        return sensitivity;
-    }
-
-    @Override
-    public boolean isReady(Character self) {
-        return true;
     }
 
     @Override public JsonObject save() {
@@ -85,75 +26,14 @@ public class EarsPart extends EarPart implements BodyPartMod {
         obj.addProperty("enum", TYPE);
         return obj;
     }
-
-    @Override
-    public double applyBonuses(Character self, Character opponent, BodyPart target, double damage, Combat c) {
-        return 0;
-    }
-
-    @Override
-    public String getFluids(Character c) {
-        return "";
-    }
-
-    @Override
-    public boolean isErogenous() {
-        return false;
-    }
-
     @Override
     public boolean isNotable() {
         return false;
     }
 
-    @Override
-    public double applyReceiveBonuses(Character self, Character opponent, BodyPart target, double damage, Combat c) {
-        return 0;
-    }
-
-    @Override
-    public BodyPart upgrade() {
-        return this;
-    }
-
-    @Override
-    public BodyPart downgrade() {
-        return this;
-    }
-
-    @Override
-    public String prefix() {
-        return "";
-    }
-
-    @Override
-    public int compare(BodyPart other) {
-        return 0;
-    }
-
-    @Override
-    public boolean isVisible(Character c) {
-        return true;
-    }
-
-    @Override
-    public double applySubBonuses(Character self, Character opponent, BodyPart with, BodyPart target, double damage,
-        Combat c) {
-        return 0;
-    }
 
     @Override
     public int mod(Attribute a, int total) {
-        return 0;
-    }
-
-    @Override
-    public void tickHolding(Combat c, Character self, Character opponent, BodyPart otherOrgan) {
-
-    }
-
-    @Override
-    public int counterValue(BodyPart otherPart, Character self, Character other) {
         return 0;
     }
 
@@ -167,8 +47,4 @@ public class EarsPart extends EarPart implements BodyPartMod {
         return TYPE;
     }
 
-    @Override
-    public String adjective() {
-        return "otic";
-    }
 }
