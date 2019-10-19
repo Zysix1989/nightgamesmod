b6568f06c9e5d56a70aab602057e07dac66c42bb
inproto451 <in.proto451@gmail.com>
2019-10-18 22:25:05 -0400

    Delete code that is never used

--- NightgamesMod/nightgames/combat/CombatListener.java
+++ NightgamesMod/nightgames/combat/CombatListener.java
@@ -1,12 +1,8 @@
 package nightgames.combat;
 
 import java.util.Optional;
-
 import nightgames.characters.Character;
-import nightgames.characters.body.BodyPart;
 import nightgames.pet.PetCharacter;
-import nightgames.skills.Skill;
-import nightgames.stance.Position;
 
 public abstract class CombatListener {
 
@@ -15,19 +11,11 @@ public abstract class CombatListener {
     public CombatListener(Combat c) {
         this.c = c;
     }
-    
-    public void preStart() {
-        
-    }
-    
+
     public void prePetBattle(PetCharacter a, PetCharacter b) {
         
     }
 
-    public void postTurn() {
-        
-    }
-    
     public void postEnd(Optional<Character> winner) {
         
     }
