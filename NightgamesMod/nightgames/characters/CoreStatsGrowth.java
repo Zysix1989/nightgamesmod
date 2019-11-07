9079b6e44d10a4550b35ff2a206519bdb126793f
inproto451 <in.proto451@gmail.com>
2019-11-07 08:29:18 -0500

    Start to lock down this class

--- NightgamesMod/nightgames/characters/CoreStatsGrowth.java
+++ NightgamesMod/nightgames/characters/CoreStatsGrowth.java
@@ -5,7 +5,7 @@ import nightgames.characters.corestats.StaminaStat;
 import nightgames.characters.corestats.WillpowerStat;
 
 public class CoreStatsGrowth {
-    public CoreStatGrowth<StaminaStat> stamina = new CoreStatGrowth<>();
-    public CoreStatGrowth<ArousalStat> arousal = new CoreStatGrowth<>();
-    public CoreStatGrowth<WillpowerStat> willpower = new CoreStatGrowth<>();
+    CoreStatGrowth<StaminaStat> stamina = new CoreStatGrowth<>();
+    CoreStatGrowth<ArousalStat> arousal = new CoreStatGrowth<>();
+    CoreStatGrowth<WillpowerStat> willpower = new CoreStatGrowth<>();
 }
