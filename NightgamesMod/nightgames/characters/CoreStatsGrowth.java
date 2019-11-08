a0a213f0b60f9099efa40b9ccbd21e37bcecfdce
inproto451 <in.proto451@gmail.com>
2019-11-07 19:54:57 -0500

    Put static code above instance code

--- NightgamesMod/nightgames/characters/CoreStatsGrowth.java
+++ NightgamesMod/nightgames/characters/CoreStatsGrowth.java
@@ -5,6 +5,13 @@ import nightgames.characters.corestats.StaminaStat;
 import nightgames.characters.corestats.WillpowerStat;
 
 public class CoreStatsGrowth {
+    static CoreStatsGrowth newDefault() {
+        var stamina = new CoreStatGrowth<StaminaStat>(2, 2);
+        var arousal = new CoreStatGrowth<ArousalStat>(4, 3);
+        var willpower = new CoreStatGrowth<WillpowerStat>(1.0f, .25f);
+        return new CoreStatsGrowth(stamina, arousal, willpower);
+    }
+
     final CoreStatGrowth<StaminaStat> stamina;
     final CoreStatGrowth<ArousalStat> arousal;
     final CoreStatGrowth<WillpowerStat> willpower;
@@ -16,11 +23,4 @@ public class CoreStatsGrowth {
         this.arousal = arousal;
         this.willpower = willpower;
     };
-
-    static CoreStatsGrowth newDefault() {
-        var stamina = new CoreStatGrowth<StaminaStat>(2, 2);
-        var arousal = new CoreStatGrowth<ArousalStat>(4, 3);
-        var willpower = new CoreStatGrowth<WillpowerStat>(1.0f, .25f);
-        return new CoreStatsGrowth(stamina, arousal, willpower);
-    }
 }
