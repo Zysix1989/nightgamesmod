b96f20776aadbe3045aa12c384de340ed829b2bc
inproto451 <in.proto451@gmail.com>
2019-11-14 08:25:27 -0500

    Convert Angel summoning to templates

--- NightgamesMod/nightgames/traits/Apostles.java
+++ NightgamesMod/nightgames/traits/Apostles.java
@@ -1,6 +1,5 @@
 package nightgames.traits;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,21 +11,29 @@ import nightgames.characters.Sarah;
 import nightgames.combat.Combat;
 import nightgames.global.Global;
 import nightgames.pet.CharacterPet;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 public class Apostles {
     private static final String APOSTLES_COUNT_FLAG = "APOSTLES_COUNT";
-    private static final List<String> ANGEL_APOSTLES_QUOTES = Arrays.asList(
-                    "The space around {self:name-do} starts abruptly shimmering. "
-                    + "{other:SUBJECT-ACTION:look|looks} up in alarm, but {self:subject} just chuckles. "
-                    + "<i>\"{other:NAME}, a Goddess should have followers don't you agree? Let's see how you fare in a ménage-à-trois, yes?\"</i>",
-                    "A soft light starts growing around {self:name-do}, causing {other:subject} to pause. "
-                    + "{self:SUBJECT} holds up her arms as if to welcome someone. "
-                    + "<i>\"Sex with just two is just so <b>lonely</b> don't you think? Let's spice it up a bit!\"</i>",
-                    "Suddenly, several pillars of light descend from the sky and converge in front of {self:name-do} in the form of a humanoid figure. "
-                    + "Not knowing what's going on, {other:subject-action:cautiously approach|cautiously approaches}. "
-                    + "{self:SUBJECT} reaches into the light and holds the figure's hands. "
-                    + "<i>\"See {other:name}, I'm not a greedy {self:girl}. I can share with my friends.\"</i>"
-                    );
+    private static final List<JtwigTemplate> SUMMON_APOSTLE_QUOTE = List.of(
+        JtwigTemplate.inlineTemplate(
+        "The space around {{ self.nameDirectObject() }} starts abruptly shimmering. "
+            + "{{ opponent.subject() }} {{ opponent.action('look') }} up in alarm, but "
+            + "{{ self.subject() }} just chuckles. "
+            + "<i>\"{{ other.getName() }}, a Goddess should have followers don't you agree? Let's "
+            + "see how you fare in a ménage-à-trois, yes?\"</i>"),
+            JtwigTemplate.inlineTemplate("A soft light starts growing around "
+                    + "{{ self.nameDirectObject() }}, causing {{ other.objectPronoun() }} to pause. "
+            + "{{ self.subject() }} holds up her arms as if to welcome someone. "
+            + "<i>\"Sex with just two is just so <b>lonely</b> don't you think? Let's spice it up a bit!\"</i>"),
+        JtwigTemplate.inlineTemplate("Suddenly, several pillars of light descend from the sky "
+            + "and converge in front of {{ self.nameDirectObject() }} in the form of a humanoid figure. "
+            + "Not knowing what's going on, {{ other.subject() }} cautiously "
+            + "{{ other.action('approach', 'approaches') }}. "
+            + "{{ self.subject() }} reaches into the light and holds the figure's hands. "
+            + "<i>\"See {{ other.getName() }}, I'm not a greedy {{ self.getGuyOrGirl }}. I can "
+            + "share with my friends.\"</i>"));
 
     public static void eachCombatRound(Combat combat, Character character, Character opponent) {
         if (character.canRespond()
@@ -36,13 +43,15 @@ public class Apostles {
                 .collect(Collectors.toList());
             var targetApostle = Global.pickRandom(possibleApostles);
             if (targetApostle.isPresent()) {
-                var quote = Global.pickRandom(ANGEL_APOSTLES_QUOTES);
-                assert quote.isPresent();
+                var quote = Global.pickRandom(SUMMON_APOSTLE_QUOTE);
                 CharacterPet pet = new CharacterPet(character,
                     targetApostle.get().getCharacter(),
                     character.getLevel() - 5,
                     character.getLevel()/4);
-                combat.write(character, Global.format(quote.get(), character, opponent));
+                var model = JtwigModel.newModel()
+                    .with("self", character)
+                    .with("opponent", opponent);
+                combat.write(character, quote.orElseThrow().render(model));
                 combat.addPet(character, pet.getSelf());
                 combat.getCombatantData(character).setIntegerFlag(APOSTLES_COUNT_FLAG, 0);
             }
