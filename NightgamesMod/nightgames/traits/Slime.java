066d5f9f6fe404d81f6ade7a15487642e9f0df3f
inproto451 <in.proto451@gmail.com>
2020-01-05 08:58:21 -0500

    Extract Slime pain effects from Character

--- NightgamesMod/nightgames/traits/Slime.java
+++ NightgamesMod/nightgames/traits/Slime.java
@@ -1,5 +1,16 @@
 package nightgames.traits;
 
-public class Slime {
+import nightgames.grammar.Person;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
+public class Slime {
+    public static int painModifier(int pain) { return -pain / 2; }
+    public static String textOnPain(Person target) {
+        var model = JtwigModel.newModel().with("target", target);
+        return ON_PAIN.render(model);
+    }
+    private static final JtwigTemplate ON_PAIN = JtwigTemplate.inlineTemplate(
+            "The blow glances off {{ target.possessivePronoun() }} slimy body."
+    );
 }
