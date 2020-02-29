4e389a45f126878772ac9299e0a8b5d1a42b7913
inproto451 <in.proto451@gmail.com>
2020-02-28 20:12:43 -0800

    Move more location based movements to factories
    This is everything but traps.  Traps require an additional call
    to SetTrap that I need to figure my way around.

--- NightgamesMod/nightgames/areas/ActionFactory.java
+++ NightgamesMod/nightgames/areas/ActionFactory.java
@@ -2,8 +2,11 @@ package nightgames.areas;
 
 import nightgames.actions.Action;
 import nightgames.characters.Character;
+import nightgames.match.Participant;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public interface ActionFactory {
     Optional<Action> createActionFor(Character c);
@@ -91,4 +94,94 @@ public interface ActionFactory {
             return Optional.empty();
         }
     }
+
+    class Bathe implements ActionFactory {
+        @Override
+        public Optional<Action> createActionFor(Character c) {
+            var action = new nightgames.actions.Bathe();
+            if (action.usable(c)) {
+                return Optional.of(action);
+            }
+            return Optional.empty();
+        }
+    }
+
+    class Craft implements ActionFactory {
+        @Override
+        public Optional<Action> createActionFor(Character c) {
+            var action = new nightgames.actions.Craft();
+            if (action.usable(c)) {
+                return Optional.of(action);
+            }
+            return Optional.empty();
+        }
+    }
+
+    class Energize implements ActionFactory {
+        @Override
+        public Optional<Action> createActionFor(Character c) {
+            var action = new nightgames.actions.Energize();
+            if (action.usable(c)) {
+                return Optional.of(action);
+            }
+            return Optional.empty();
+        }
+    }
+
+    class Hide implements ActionFactory {
+        @Override
+        public Optional<Action> createActionFor(Character c) {
+            var action = new nightgames.actions.Hide();
+            if (action.usable(c)) {
+                return Optional.of(action);
+            }
+            return Optional.empty();
+        }
+    }
+
+    class Recharge implements ActionFactory {
+        @Override
+        public Optional<Action> createActionFor(Character c) {
+            var action = new nightgames.actions.Recharge();
+            if (action.usable(c)) {
+                return Optional.of(action);
+            }
+            return Optional.empty();
+        }
+    }
+
+    class Resupply implements ActionFactory {
+        boolean permissioned;
+        Set<Character> allowedCharacters;
+
+        public Resupply() {
+            permissioned = false;
+        }
+
+        public Resupply(Set<Participant> participants) {
+            permissioned = true;
+            allowedCharacters = participants.stream().map(Participant::getCharacter).collect(Collectors.toSet());
+        }
+
+        @Override
+        public Optional<Action> createActionFor(Character c) {
+            var action = new nightgames.actions.Resupply();
+            if (action.usable(c) && (!permissioned || allowedCharacters.contains(c))) {
+                return Optional.of(action);
+            }
+            return Optional.empty();
+        }
+    }
+
+    class Scavenge implements ActionFactory {
+        @Override
+        public Optional<Action> createActionFor(Character c) {
+            var action = new nightgames.actions.Scavenge();
+            if (action.usable(c)) {
+                return Optional.of(action);
+            }
+            return Optional.empty();
+        }
+    }
+
 }
