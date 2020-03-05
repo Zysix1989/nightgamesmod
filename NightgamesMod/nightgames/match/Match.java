6005b9077161e2855b6d01bc1a49466ced0e68b5
inproto451 <in.proto451@gmail.com>
2020-03-05 14:15:27 -0800

    Remove dead code

--- NightgamesMod/nightgames/match/Match.java
+++ NightgamesMod/nightgames/match/Match.java
@@ -20,51 +20,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Match {
-
-    public static final String QUAD_DESCRIPTION = "You are in the <b>Quad</b> that sits in the center of the Dorm, the Dining Hall, the Engineering Building, and the Liberal Arts Building. There's "
-            + "no one around at this time of night, but the Quad is well-lit and has no real cover. You can probably be spotted from any of the surrounding buildings, it may "
-            + "not be a good idea to hang out here for long.";
-    public static final String DORM_DESCRIPTION = "You are in the <b>Dorm</b>. Everything is quieter than it would be in any other dorm this time of night. You've been told the entire first floor "
-            + "is empty during match hours, but you wouldn't be surprised if a few of the residents are hiding in their rooms, peeking at the fights. You've stashed some clothes "
-            + "in one of the rooms you're sure is empty, which is common practice for most of the competitors.";
-    public static final String SHOWER_DESCRIPTION = "You are in the first floor <b>Showers</b>. There are a half-dozen stalls shared by the residents on this floor. They aren't very big, but there's "
-            + "room to hide if need be. A hot shower would help you recover after a tough fight, but you'd be vulnerable if someone finds you.";
-    public static final String LAUNDRY_DESCRIPTION = "You are in the <b>Laundry Room</b> in the basement of the Dorm. Late night is prime laundry time in your dorm, but none of these machines "
-            + "are running. You're a bit jealous when you notice that the machines here are free, while yours are coin-op. There's a tunnel here that connects to the basement of the "
-            + "Dining Hall.";
-    public static final String ENGINEERING_DESCRIPTION = "You are in the Science and <b>Engineering Building</b>. Most of the lecture rooms are in other buildings; this one is mostly "
-            + "for specialized rooms and labs. The first floor contains workshops mostly used by the Mechanical and Electrical Engineering classes. The second floor has "
-            + "the Biology and Chemistry Labs. There's a third floor, but that's considered out of bounds.";
-    public static final String LAB_DESCRIPTION = "You are in the <b>Chemistry Lab</b>. The shelves and cabinets are full of all manner of dangerous and/or interesting chemicals. A clever enough "
-            + "person could combine some of the safer ones into something useful. Just outside the lab is a bridge connecting to the library.";
-    public static final String WORKSHOP_DESCRIPTION = "You are in the Mechanical Engineering <b>Workshop</b>. There are shelves of various mechanical components and the back table is covered "
-            + "with half-finished projects. A few dozen Mechanical Engineering students use this workshop each week, but it's well stocked enough that no one would miss "
-            + "some materials that might be of use to you.";
-    public static final String LIBERAL_ARTS_DESCRIPTION = "You are in the <b>Liberal Arts Building</b>. There are three floors of lecture halls and traditional classrooms, but only "
-            + "the first floor is in bounds. The Library is located directly out back, and the side door is just a short walk from the pool.";
-    public static final String POOL_DESCRIPTION = "You are by the indoor <b>Pool</b>, which is connected to the Student Union for reasons that no one has ever really explained. The pool here is quite "
-            + "large and there is even a jacuzzi. A quick soak would feel good, but the lack of privacy is a concern. The side doors are locked at this time of night, but the "
-            + "door to the Student Union is open and there's a back door that exits near the Liberal Arts building. Across the water in the other direction is the Courtyard.";
-    public static final String LIBRARY_DESCRIPTION = "You are in the <b>Library</b>. It's a two floor building with an open staircase connecting the first and second floors. The front entrance leads to "
-            + "the Liberal Arts building. The second floor has a Bridge connecting to the Chemistry Lab in the Science and Engineering building.";
-    public static final String DINING_HALL_DESCRIPTION = "You are in the <b>Dining Hall</b>. Most students get their meals here, though some feel it's worth the extra money to eat out. The "
-            + "dining hall is quite large and your steps echo on the linoleum, but you could probably find someplace to hide if you need to.";
-    public static final String KITCHEN_DESCRIPTION = "You are in the <b>Kitchen</b> where student meals are prepared each day. The industrial fridge and surrounding cabinets are full of the "
-            + "ingredients for any sort of bland cafeteria food you can imagine. Fortunately, you aren't very hungry. There's a chance you might be able to cook up some "
-            + "of the more obscure items into something useful.";
-    public static final String STORAGE_DESCRIPTION = "You are in a <b>Storage Room</b> under the Dining Hall. It's always unlocked and receives a fair bit of foot traffic from students "
-            + "using the tunnel to and from the Dorm, so no one keeps anything important in here. There's enough junk down here to provide some hiding places and there's a chance "
-            + "you could find something useable in one of these boxes.";
-    public static final String TUNNEL_DESCRIPTION = "You are in the <b>Tunnel</b> connecting the dorm to the dining hall. It doesn't get a lot of use during the day and most of the freshmen "
-            + "aren't even aware of its existence, but many upperclassmen have been thankful for it on cold winter days and it's proven to be a major tactical asset. The "
-            + "tunnel is well-lit and doesn't offer any hiding places.";
-    public static final String BRIDGE_DESCRIPTION = "You are on the <b>Bridge</b> connecting the second floors of the Science and Engineering Building and the Library. It's essentially just a "
-            + "corridor, so there's no place for anyone to hide.";
-    public static final String STUDENT_UNION_DESCRIPTION = "You are in the <b>Student Union</b>, which doubles as base of operations during match hours. You and the other competitors can pick up "
-            + "a change of clothing here.";
-    public static final String COURTYARD_DESCRIPTION = "You are in the <b>Courtyard</b>. "
-            + "It's a small clearing behind the school pool. There's not much to see here except a tidy garden maintained by the botany department.";
-
     protected int time;
     private int timeSinceLastDrop;
     protected Map<String, Area> map;
