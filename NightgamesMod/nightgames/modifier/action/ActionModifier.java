ed9fcf4a0b4a2583e3fe5acad3fa449eaad19987
inproto451 <in.proto451@gmail.com>
2020-03-14 19:19:18 -0700

    Reduce anonymous instance to base class

--- NightgamesMod/nightgames/modifier/action/ActionModifier.java
+++ NightgamesMod/nightgames/modifier/action/ActionModifier.java
@@ -2,7 +2,7 @@ package nightgames.modifier.action;
 
 import nightgames.match.Action;
 
-public abstract class ActionModifier {
+public class ActionModifier {
 
     public boolean actionIsBanned(Action act) {
         return false;
--- NightgamesMod/nightgames/modifier/action/ActionModifierCombiner.java
+++ NightgamesMod/nightgames/modifier/action/ActionModifierCombiner.java
@@ -4,13 +4,7 @@ package nightgames.modifier.action;
  * TODO: Write class-level documentation.
  */
 public final class ActionModifierCombiner {
-    public static final ActionModifier NULL_MODIFIER = new ActionModifier() {
-
-        @Override public String toString() {
-            return "null-action-modifier";
-        }
-
-    };
+    public static final ActionModifier NULL_MODIFIER = new ActionModifier();
 
     private ActionModifierCombiner() {
     }
