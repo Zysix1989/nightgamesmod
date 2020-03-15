de4e31c5f2512c3350d5616c69c2bc407f02a07f
inproto451 <in.proto451@gmail.com>
2020-03-14 21:35:21 -0700

    Support proper stringing during Predicate helpers.
    I'd like to be able to call these and retain the useful string-ability
    of these predicates.

--- NightgamesMod/nightgames/modifier/action/BanActionPredicate.java
+++ NightgamesMod/nightgames/modifier/action/BanActionPredicate.java
@@ -19,6 +19,29 @@ public class BanActionPredicate implements Predicate<Action> {
     }
 
     @Override
+    public Predicate<Action> and(Predicate<? super Action> other) {
+        var result = Predicate.super.and(other);
+        if (other instanceof BanActionPredicate) {
+            return new BanActionPredicate("(and " + toString() + " " + other.toString() + ")", result);
+        }
+        return result;
+    }
+
+    @Override
+    public BanActionPredicate negate() {
+        return new BanActionPredicate("(not " + toString() + ")", Predicate.super.negate());
+    }
+
+    @Override
+    public Predicate<Action> or(Predicate<? super Action> other) {
+        var result = Predicate.super.or(other);
+        if (other instanceof BanActionPredicate) {
+            return new BanActionPredicate("(or " + toString() + " " + other.toString() + ")", result);
+        }
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "(not " + description + ")";
     }
