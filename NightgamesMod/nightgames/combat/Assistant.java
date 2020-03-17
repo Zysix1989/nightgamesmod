8a1b8ec2789f3a4306932493680df8fa65b79756
inproto451 <in.proto451@gmail.com>
2020-03-17 12:47:40 -0700

    Format for readability

--- NightgamesMod/nightgames/combat/Assistant.java
+++ NightgamesMod/nightgames/combat/Assistant.java
@@ -63,13 +63,19 @@ public class Assistant {
     public boolean act(Combat c, Character target) {
         List<Skill> allowedEnemySkills = character.getSkills()
                 .stream()
-                .filter(skill -> Skill.isUsableOn(c, skill, target) && Collections.disjoint(skill.getTags(c), PET_UNUSABLE_TAG)).collect(Collectors.toList());
+                .filter(skill -> Skill.isUsableOn(c, skill, target) &&
+                        Collections.disjoint(skill.getTags(c), PET_UNUSABLE_TAG))
+                .collect(Collectors.toList());
         Skill.filterAllowedSkills(c, allowedEnemySkills, character, target);
 
         List<Skill> allowedMasterSkills = character.getSkills()
-                .stream().filter(skill -> Skill.isUsableOn(c, skill, character.getSelf().owner)
-                        && (skill.getTags(c).contains(SkillTag.helping) || (character.getSelf().owner.has(Trait.showmanship) && skill.getTags(c).contains(SkillTag.worship)))
-                        && Collections.disjoint(skill.getTags(c), PET_UNUSABLE_TAG)).collect(Collectors.toList());
+                .stream()
+                .filter(skill -> Skill.isUsableOn(c, skill, character.getSelf().owner) &&
+                        (skill.getTags(c).contains(SkillTag.helping) ||
+                                (character.getSelf().owner.has(Trait.showmanship) &&
+                                        skill.getTags(c).contains(SkillTag.worship))) &&
+                        Collections.disjoint(skill.getTags(c), PET_UNUSABLE_TAG))
+                .collect(Collectors.toList());
         Skill.filterAllowedSkills(c, allowedMasterSkills, character, character.getSelf().owner);
         WeightedSkill bestEnemySkill = Decider.prioritizePet(character, target, allowedEnemySkills, c);
         WeightedSkill bestMasterSkill = Decider.prioritizePet(character, character.getSelf().owner, allowedMasterSkills, c);
