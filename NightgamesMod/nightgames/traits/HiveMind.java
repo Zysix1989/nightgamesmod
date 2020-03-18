5b96223085996753d6f8d0635e30664d0347194a
inproto451 <in.proto451@gmail.com>
2020-03-18 08:31:06 -0700

    HiveMind.resolveOrgasm: getPetsFor() -> assistantsOf()
    Use the preferred alternative

--- NightgamesMod/nightgames/traits/HiveMind.java
+++ NightgamesMod/nightgames/traits/HiveMind.java
@@ -11,7 +11,7 @@ public final class HiveMind {
 
     // Returns true if this should end the orgasm
     public static boolean resolveOrgasm(Combat c, Character bearer, Character opponent) {
-        if (!c.getPetsFor(bearer).isEmpty()) {
+        if (!c.assistantsOf(bearer).isEmpty()) {
             // don't use opponent, use opponent of the current combat
             c.write(bearer, Global.format("Just as {self:subject-action:seem} about to "
                             + "orgasm, {self:possessive} expression shifts. {self:POSSESSIVE} eyes dulls "
@@ -19,13 +19,15 @@ public final class HiveMind {
                             + "{other:if-human: Shit you've seen this before, she somehow switched "
                             + "bodies with one of her clones!}",
                     bearer, c.getOpponentCharacter(bearer)));
-            c.getPetsFor(bearer).forEach(assistant -> {
+            c.assistantsOf(bearer).forEach(assistant -> {
                 if (bearer.checkOrgasm()) {
                     int amount = Math.min(bearer.getArousal().get(),
-                            Math.max(0, assistant.getArousal().max() - assistant.getArousal().get()));
+                            Math.max(0,
+                                    assistant.getCharacter().getArousal().max() -
+                                            assistant.getCharacter().getArousal().get()));
                     bearer.getArousal().calm(amount);
-                    assistant.arouse(amount, c, Global.format("({self:master}'s orgasm)", bearer, opponent));
-                    assistant.doOrgasm(c, assistant, null, null);
+                    assistant.getCharacter().arouse(amount, c, Global.format("({self:master}'s orgasm)", bearer, opponent));
+                    assistant.getCharacter().doOrgasm(c, assistant.getCharacter(), null, null);
                 }
             });
             c.setStance(new Neutral(bearer, c.getOpponentCharacter(bearer)));
