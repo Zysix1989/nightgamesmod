43f9dc964ced9fe7dbce9c44f619fcf76c27041d
inproto451 <in.proto451@gmail.com>
2020-03-21 13:25:28 -0700

    Male.vanquish(): Extract helper method to clarify
    This separates the concerns of generating the scene with the gameplay
    effects.

--- NightgamesMod/nightgames/pet/ImpMale.java
+++ NightgamesMod/nightgames/pet/ImpMale.java
@@ -11,11 +11,9 @@ import nightgames.skills.Grind;
 import nightgames.skills.Piston;
 import nightgames.skills.PussyGrind;
 import nightgames.skills.Thrust;
-import nightgames.skills.petskills.ImpAssault;
-import nightgames.skills.petskills.ImpFacesit;
-import nightgames.skills.petskills.ImpSemenSquirt;
-import nightgames.skills.petskills.ImpStrip;
-import nightgames.skills.petskills.ImpTease;
+import nightgames.skills.petskills.*;
+
+import java.util.Optional;
 
 public class ImpMale extends Pet {
 
@@ -35,36 +33,39 @@ public class ImpMale extends Pet {
 
     @Override
     public void vanquish(Combat c, Pet opponent) {
+        var instakillScene = instakillScene(opponent);
+        if (instakillScene.isPresent()) {
+            c.write(getSelf(), instakillScene.get());
+            c.removePet(opponent.getSelf());
+        } else {
+            (new ImpTease(getSelf())).resolve(c, opponent.getSelf());
+        }
+    }
+
+    public Optional<String> instakillScene(Pet opponent) {
         switch (opponent.type()) {
             case fairyfem:
-                c.write(getSelf(), own() + "imp manages to catch " + opponent.own()
+                return Optional.of(own() + "imp manages to catch " + opponent.own()
                                 + "faerie as the tiny fae flies around his head. He shoves his cock into the faerie's face, smearing his pre-cum over her. "
                                 + "He presses her against his shaft, stroking himself with her entire body. The faerie, intoxicated by the potent fluid is unable to last long and cums with a high "
                                 + "pitched moan.");
-                break;
             case fairymale:
-                c.write(getSelf(), own() + "imp swats " + opponent.own() + "faerie out of the air.");
-                break;
+                return Optional.of(own() + "imp swats " + opponent.own() + "faerie out of the air.");
             case impfem:
-                c.write(getSelf(), own() + "imp grabs " + opponent.own()
+                return Optional.of(own() + "imp grabs " + opponent.own()
                                 + "female imp and bends her over. He rams his cock into her wet box without any foreplay. The female groans in protest and flails about "
                                 + "in an attempt to take control, but she's held fast. The male shows impressive stamina, fucking the female until she orgasms and vanishes right off his dick. He then seeks a "
                                 + "new target for his unsatisfied cock.");
-                break;
             case impmale:
-                c.write(getSelf(), "");
-                break;
+                return Optional.of("");
             case slime:
-                c.write(getSelf(), own() + "imp pins " + opponent.own()
+                return Optional.of(own() + "imp pins " + opponent.own()
                                 + "slime under foot and lets his leaking cock drip onto the amorphous mass. As the slime absorbs the first drops of pre-cum, it starts to "
                                 + "frantically attempt to reach the imp's penis. It seems unable to change shape though and the demon keeps it pinned while letting more pre-cum drip down. After absorbing enough "
                                 + "fluid, the slime's color darkens and it gradually solidifies, unable to move.");
-                break;
             default:
-                (new ImpTease(getSelf())).resolve(c, opponent.getSelf());
-                return;
+                return Optional.empty();
         }
-        c.removePet(opponent.getSelf());
     }
 
     @Override
