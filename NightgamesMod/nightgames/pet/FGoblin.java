23614a53ede707cd658f8905b078ab248d931357
inproto451 <in.proto451@gmail.com>
2020-03-21 13:18:22 -0700

    FGoblin.vanquish(): Extract helper method to clarify
    This separates the concerns of generating the scene with the gameplay
    effects.

--- NightgamesMod/nightgames/pet/FGoblin.java
+++ NightgamesMod/nightgames/pet/FGoblin.java
@@ -9,11 +9,9 @@ import nightgames.skills.Grind;
 import nightgames.skills.Piston;
 import nightgames.skills.PussyGrind;
 import nightgames.skills.Thrust;
-import nightgames.skills.petskills.GoblinBondage;
-import nightgames.skills.petskills.GoblinDenial;
-import nightgames.skills.petskills.GoblinFaceFuck;
-import nightgames.skills.petskills.GoblinMasochism;
-import nightgames.skills.petskills.GoblinTease;
+import nightgames.skills.petskills.*;
+
+import java.util.Optional;
 
 public class FGoblin extends Pet {
     public FGoblin(Character owner, int pow, int ac) {
@@ -27,45 +25,47 @@ public class FGoblin extends Pet {
 
     @Override
     public void vanquish(Combat c, Pet opponent) {
+        var instakillScene = instakillScene(opponent);
+        if (instakillScene.isPresent()) {
+            c.write(getSelf(), instakillScene.get());
+            c.removePet(opponent.getSelf());
+        } else {
+            (new GoblinTease(getSelf())).resolve(c, opponent.getSelf());
+        }
+    }
+
+    public Optional<String> instakillScene(Pet opponent) {
         switch(opponent.type()){
             case fairyfem:
-                c.write(getSelf(),String.format("%s faerie girl flies low, aiming for %s fetish goblin's weak spot, but the goblin knocks the little fae out of the air "
+                return Optional.of(String.format("%s faerie girl flies low, aiming for %s fetish goblin's weak spot, but the goblin knocks the little fae out of the air "
                         + "with a swing of her hefty girl-cock. The fetish goblin grabs the dazed faerie and finishes her off by pressing a vibrator against her tiny "
                         + "slit.",opponent.own(),own()));
-                break;
             case fairymale:
-                c.write(getSelf(),String.format("%s fetish goblin manages to catch %s faerie as he carelessly flies too close. She shoves the tiny male between her heavy "
+                return Optional.of(String.format("%s fetish goblin manages to catch %s faerie as he carelessly flies too close. She shoves the tiny male between her heavy "
                         + "boobs, completely engulfing him within her cleavage. You don't know if or when he desummoned, but he's clearly not coming out of there.", own(),opponent.own()));
-                break;
             case impfem:
-                c.write(getSelf(),String.format("%s fetish goblin overpowers %s imp girl and quickly binds her wrists. The goblin bends the helpless imp over her knee "
+                return Optional.of(String.format("%s fetish goblin overpowers %s imp girl and quickly binds her wrists. The goblin bends the helpless imp over her knee "
                         + "and slides a suspiciously wet vibrator into her exposes pussy. The imp moans and squirms, but is unable to get away. The goblin punishes the "
                         + "escape attempt with ten hard slaps on the imp's upturned ass. By the final slap, the imp is practically screaming in orgasm and quickly vanishes "
                         + "in a puff of brimstone.",own(),opponent.own()));
-                break;
             case impmale:
-                c.write(getSelf(),String.format("%s fetish goblin and %s imp grapple with each other, vying for dominance. The larger goblin gains the upper hand and "
-                        + "staggers the imp with a quick knee to the groin. The imp doubles over in pain, giving the hermaphroditic goblin time to get behind him. She "
-                        + "lines up her cock with the imp's unprotected ass and penetrates him with a firm thrust. %s imp squeals in alarm as being suddenly fucked "
-                        + "from behind. The goblin pegs the imp steadily and reaches around to stroke his cock. Soon, the imp sprays cum into the air and disappears.",
+                return Optional.of(String.format("%s fetish goblin and %s imp grapple with each other, vying for dominance. The larger goblin gains the upper hand and "
+                                + "staggers the imp with a quick knee to the groin. The imp doubles over in pain, giving the hermaphroditic goblin time to get behind him. She "
+                                + "lines up her cock with the imp's unprotected ass and penetrates him with a firm thrust. %s imp squeals in alarm as being suddenly fucked "
+                                + "from behind. The goblin pegs the imp steadily and reaches around to stroke his cock. Soon, the imp sprays cum into the air and disappears.",
                         own(),opponent.own(),opponent.own()));
-                break;
             case slime:
-                c.write(getSelf(),String.format("%s fetish goblin removes her boots and steps barefoot into %s slime. The amorphous creature squirms happily around the "
+                return Optional.of(String.format("%s fetish goblin removes her boots and steps barefoot into %s slime. The amorphous creature squirms happily around the "
                         + "goblin's feet, but makes no attempt to attack. It seems to be completely fascinated with her feet. The goblin wiggles her toes and the slime "
                         + "trembles with delight, before melting into a content puddle.", own(),opponent.own()));
-                break;
             case fgoblin:
-                c.write(getSelf(),String.format("%s fetish goblin tackles %s goblin, pinning her arms behind her back. %s goblin ties up the other, using some spare ropes "
-                        + "and bandage straps. The poor, bound herm is left completely immobile and vulnerable, but seems to be getting very aroused by her situation. The "
-                        + "dominant fetish goblin takes her time getting her helpless opponent off, but eventually %s goblin is whimpering and twitching in orgams",
+                return Optional.of(String.format("%s fetish goblin tackles %s goblin, pinning her arms behind her back. %s goblin ties up the other, using some spare ropes "
+                                + "and bandage straps. The poor, bound herm is left completely immobile and vulnerable, but seems to be getting very aroused by her situation. The "
+                                + "dominant fetish goblin takes her time getting her helpless opponent off, but eventually %s goblin is whimpering and twitching in orgams",
                         own(),opponent.own(),own(),opponent.own()));
-                break;
             default:
-                (new GoblinTease(getSelf())).resolve(c, opponent.getSelf());
-                return;
-            }
-        c.removePet(opponent.getSelf());
+                return Optional.empty();
+        }
     }
 
     @Override
