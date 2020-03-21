2196200e606e725ec9ffaae109f51639b07b26e9
inproto451 <in.proto451@gmail.com>
2020-03-21 13:25:17 -0700

    ImpFem.vanquish(): Extract helper method to clarify
    This separates the concerns of generating the scene with the gameplay
    effects.

--- NightgamesMod/nightgames/pet/ImpFem.java
+++ NightgamesMod/nightgames/pet/ImpFem.java
@@ -10,11 +10,9 @@ import nightgames.skills.Grind;
 import nightgames.skills.Piston;
 import nightgames.skills.PussyGrind;
 import nightgames.skills.Thrust;
-import nightgames.skills.petskills.ImpAssault;
-import nightgames.skills.petskills.ImpFacesit;
-import nightgames.skills.petskills.ImpSemenSquirt;
-import nightgames.skills.petskills.ImpStrip;
-import nightgames.skills.petskills.ImpTease;
+import nightgames.skills.petskills.*;
+
+import java.util.Optional;
 
 public class ImpFem extends Pet {
     public ImpFem(Character owner) {
@@ -32,42 +30,45 @@ public class ImpFem extends Pet {
 
     @Override
     public void vanquish(Combat c, Pet opponent) {
+        var instakillScene = instakillScene(opponent);
+        if (instakillScene.isPresent()) {
+            c.write(getSelf(), instakillScene.get());
+            c.removePet(opponent.getSelf());
+        } else {
+            (new ImpTease(getSelf())).resolve(c, opponent.getSelf());
+        }
+    }
+
+    public Optional<String> instakillScene(Pet opponent) {
         switch (opponent.type()) {
             case fairyfem:
-                c.write(getSelf(), own() + "imp grabs " + opponent.own()
-                                + "faerie and inserts the tiny girl into her soaking cunt. She pulls the faerie out after only a few seconds, but "
-                                + "the sprite is completely covered with the imp's aphrodisiac wetness. The demon simply watches as the horny fae girl frantically masturbates in a sex "
-                                + "drunk daze. It doesn't take long until the faerie disappears with an orgasmic moan.");
-                break;
+                return Optional.of(own() + "imp grabs " + opponent.own()
+                        + "faerie and inserts the tiny girl into her soaking cunt. She pulls the faerie out after only a few seconds, but "
+                        + "the sprite is completely covered with the imp's aphrodisiac wetness. The demon simply watches as the horny fae girl frantically masturbates in a sex "
+                        + "drunk daze. It doesn't take long until the faerie disappears with an orgasmic moan.");
             case fairymale:
-                c.write(getSelf(), own() + "imp catches " + opponent.own()
-                                + "faerie boy and holds him in her palm. The imp uses one finger to toy with the fae's tiny penis and the little "
-                                + "male squirms helplessly. She dexterously rubs the faerie until it cums on her finger and vanishes with a flash.");
-                break;
+                return Optional.of(own() + "imp catches " + opponent.own()
+                        + "faerie boy and holds him in her palm. The imp uses one finger to toy with the fae's tiny penis and the little "
+                        + "male squirms helplessly. She dexterously rubs the faerie until it cums on her finger and vanishes with a flash.");
             case impfem:
-                c.write(getSelf(), "The two female imps grapple with each other and " + opponent.own() + " imp throws "
-                                + own() + "imp to the floor. " + opponent.own() + " imp approaches to press her "
-                                + "advantage, but " + own()
-                                + "imp's tail suddenly thrusts into her pussy. As the tails fucks her, " + own()
-                                + "imp collects some of her own wetness and forces the "
-                                + "aphrodisiac filled fluid into the other female's mouth. " + opponent.own()
-                                + "imp's orgasmic moan is stifled and she vanishes in a puff of brimstone.");
-                break;
+                return Optional.of("The two female imps grapple with each other and " + opponent.own() + " imp throws "
+                        + own() + "imp to the floor. " + opponent.own() + " imp approaches to press her "
+                        + "advantage, but " + own()
+                        + "imp's tail suddenly thrusts into her pussy. As the tails fucks her, " + own()
+                        + "imp collects some of her own wetness and forces the "
+                        + "aphrodisiac filled fluid into the other female's mouth. " + opponent.own()
+                        + "imp's orgasmic moan is stifled and she vanishes in a puff of brimstone.");
             case impmale:
-                c.write(getSelf(), own() + "imp grapples with " + opponent.own()
-                                + "imp and gets a hold of his erection. She uses her leverage to parade the male around the edge of the battle. "
-                                + "She strokes the demonic dick to the edge of orgasm and then mercilessly slams her knee into his balls. The male howls in pain and disappears.");
-                break;
+                return Optional.of(own() + "imp grapples with " + opponent.own()
+                        + "imp and gets a hold of his erection. She uses her leverage to parade the male around the edge of the battle. "
+                        + "She strokes the demonic dick to the edge of orgasm and then mercilessly slams her knee into his balls. The male howls in pain and disappears.");
             case slime:
-                c.write(getSelf(), own() + "imp shoves both her hands into " + opponent.own()
-                                + "slime. The slime trembles at her touch, encouraging her to wiggle her fingers more inside its "
-                                + "semi-solid body. The slime writhes more and more before it suddenly shudders, then slowly melts into a puddle.");
-                break;
+                return Optional.of(own() + "imp shoves both her hands into " + opponent.own()
+                        + "slime. The slime trembles at her touch, encouraging her to wiggle her fingers more inside its "
+                        + "semi-solid body. The slime writhes more and more before it suddenly shudders, then slowly melts into a puddle.");
             default:
-                (new ImpTease(getSelf())).resolve(c, opponent.getSelf());
-                return;
+                return Optional.empty();
         }
-        c.removePet(opponent.getSelf());
     }
 
     @Override
